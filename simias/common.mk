c92694e34042e00300af1957f3dcc8cd9314973c
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2004-02-25 02:18:24 +0000

    updated the common.mk to handle luser DOS commands that can only copy one dang file at a time
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/MAKEREWORK-20040224@83 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/common.mk
+++ simias/common.mk
@@ -1,11 +1,5 @@
 # Setup install targets
 
-UNINSTALL_BIN_FILES := $(addprefix $(DESTDIR)$(bindir)$(SEP), $(BIN_FILES))
-UNINSTALL_LIB_FILES := $(addprefix $(DESTDIR)$(libdir)$(SEP), $(LIB_FILES))
-UNINSTALL_DATA_FILES := $(addprefix $(DESTDIR)$(datadir)$(SEP), $(DATA_FILES))
-UNINSTALL_TEST_BIN_FILES := $(addprefix $(DESTDIR)$(bindir)$(SEP), $(TEST_BIN_FILES))
-UNINSTALL_TEST_LIB_FILES := $(addprefix $(DESTDIR)$(libdir)$(SEP), $(TEST_LIB_FILES))
-UNINSTALL_TEST_DATA_FILES := $(addprefix $(DESTDIR)$(datadir)$(SEP), $(TEST_DATA_FILES))
 
 TEST_RUN_MAKEFILE := $(DESTDIR)$(bindir)$(SEP)test.mk
 
@@ -37,51 +31,93 @@ uninstall: bin_uninstall lib_uninstall data_uninstall
 
 uninstalltest: test_bin_uninstall test_lib_uninstall test_data_uninstall
 
-
-ifdef BIN_FILES
-bin_install:
+$(DESTDIR)$(bindir):
 	$(MKDIR_R) $(DESTDIR)$(bindir)
-	$(CP) $(BIN_FILES) $(DESTDIR)$(bindir)
+
+#$(DESTDIR)$(libdir):
+#	$(MKDIR_R) $(DESTDIR)$(libdir)
+
+#$(DESTDIR)$(libdir):
+#	$(MKDIR_R) $(DESTDIR)$(datadir)
+
+# this is the uglies piece of hud I have ever seen but to build on
+# a windoZzzz box, you have to copy each file individually, LAME!
+# what a luser OS!
+ifdef BIN_FILES
+UNINSTALL_BIN_FILES := $(addprefix $(DESTDIR)$(bindir)$(SEP), $(BIN_FILES))
+
+bin_install: $(DESTDIR)$(bindir) $(UNINSTALL_BIN_FILES)
+
+$(UNINSTALL_BIN_FILES): 
+	$(CP) $(subst $(DESTDIR)$(bindir)$(SEP),,$@) $(DESTDIR)$(bindir)
+
 bin_uninstall:
 	$(RM) $(UNINSTALL_BIN_FILES)
 endif
 
+
+
 ifdef LIB_FILES
-lib_install:
-	$(MKDIR_R) $(DESTDIR)$(libdir)
-	$(CP) $(LIB_FILES) $(DESTDIR)$(libdir)
+UNINSTALL_LIB_FILES := $(addprefix $(DESTDIR)$(libdir)$(SEP), $(LIB_FILES))
+
+lib_install: $(DESTDIR)$(libdir) $(UNINSTALL_LIB_FILES)
+
+$(UNINSTALL_LIB_FILES):
+	$(CP) $(subst $(DESTDIR)$(libdir)$(SEP),,$@) $(DESTDIR)$(libdir)
+
 lib_uninstall:
 	$(RM) $(UNINSTALL_LIB_FILES)
 endif
 
+
+
 ifdef DATA_FILES
-data_install:
-	$(MKDIR_R) $(DESTDIR)$(datadir)
-	$(CP) $(DATA_FILES) $(DESTDIR)$(datadir)
+UNINSTALL_DATA_FILES := $(addprefix $(DESTDIR)$(datadir)$(SEP), $(DATA_FILES))
+
+data_install: $(DESTDIR)$(datadir) $(UNINSTALL_DATA_FILES)
+
+$(UNINSTALL_LIB_FILES):
+	$(CP) $(subst $(DESTDIR)$(datadir)$(SEP),,$@) $(DESTDIR)$(datadir)
+
 data_uninstall:
 	$(RM) $(UNINSTALL_DATA_FILES)
 endif
 
+
+
 ifdef TEST_BIN_FILES
-test_bin_install:
-	$(MKDIR_R) $(DESTDIR)$(bindir)
-	$(CP) $(TEST_BIN_FILES) $(DESTDIR)$(bindir)
+UNINSTALL_TEST_BIN_FILES := $(addprefix $(DESTDIR)$(bindir)$(SEP), $(TEST_BIN_FILES))
+
+test_bin_install: $(DESTDIR)$(bindir) $(UNINSTALL_TEST_BIN_FILES)
+
+$(UNINSTALL_TEST_BIN_FILES):
+	$(CP) $(subst $(DESTDIR)$(bindir)$(SEP),,$@) $(DESTDIR)$(bindir)
+
 test_bin_uninstall:
 	$(RM) $(UNINSTALL_TEST_BIN_FILES)
 endif
 
+
 ifdef TEST_LIB_FILES
-test_lib_install:
-	$(MKDIR_R) $(DESTDIR)$(libdir)
-	$(CP) $(TEST_LIB_FILES) $(DESTDIR)$(libdir)
+UNINSTALL_TEST_LIB_FILES := $(addprefix $(DESTDIR)$(libdir)$(SEP), $(TEST_LIB_FILES))
+
+test_lib_install: $(DESTDIR)$(libdir) $(UNINSTALL_TEST_LIB_FILES)
+
+$(UNINSTALL_TEST_LIB_FILES):
+	$(CP) $(subst $(DESTDIR)$(libdir)$(SEP),,$@) $(DESTDIR)$(libdir)
+
 test_lib_uninstall:
 	$(RM) $(UNINSTALL_TEST_LIB_FILES)
 endif
 
 ifdef TEST_DATA_FILES
-test_data_install:
-	$(MKDIR_R) $(DESTDIR)$(datadir)
-	$(CP) $(TEST_DATA_FILES) $(DESTDIR)$(datadir)
+UNINSTALL_TEST_DATA_FILES := $(addprefix $(DESTDIR)$(datadir)$(SEP), $(TEST_DATA_FILES))
+
+test_data_install: $(DESTDIR)$(datadir) $(UNINSTALL_TEST_DATA_FILES)
+
+$(UNINSTALL_TEST_DATA_FILES):
+	$(CP) $(subst $(DESTDIR)$(datadir)$(SEP),,$@) $(DESTDIR)$(datadir)
+
 test_data_uninstall:
 	$(RM) $(UNINSTALL_TEST_DATA_FILES)
 endif
