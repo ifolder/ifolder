ab24a690a5f307380cbe773c1331e5da34fa014d
ryoung65 <ryoung65@db2431b9-df06-0410-971c-a38ea9a75485>
2004-02-26 23:01:07 +0000

    Changed the event system to seperate file events from Node events.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@161 db2431b9-df06-0410-971c-a38ea9a75485

---
+++ simias/src/CollectionEvents/CollectionEventArgs.cs
@@ -0,0 +1,107 @@
+/***********************************************************************
+ *  CollectionEventArgs.cs - Argument class for collection events.
+ * 
+ *  Copyright (C) 2004 Novell, Inc.
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Author: Russ Young <ryoung@novell.com>
+ * 
+ ***********************************************************************/
+
+using System;
+
+namespace Simias.Event
+{
+	/// <summary>
+	/// The event arguments for a Collection event.
+	/// </summary>
+	[Serializable]
+	public abstract class CollectionEventArgs : EventArgs
+	{
+		string					source;
+		string					id;
+		string					collection;
+		string					type;
+		EventType				changeType;
+
+		[FlagsAttribute] 
+		public enum EventType : short
+		{
+			Created = 1,
+			Deleted = 2,
+			Changed = 4,
+			Renamed = 8,
+			RootChanged = 16
+		};
+
+
+		/// <summary>
+		/// Constructs a CollectionEventArgs that will be used by CollectionHandler delegates.
+		/// Descibes the node affected by the event.
+		/// </summary>
+		/// <param name="source">The source of the event.</param>
+		/// <param name="id">The object of the event.</param>
+		/// <param name="collection">The Collection that the node belongs to.</param>
+		/// <param name="type">The Type of the Node.</param>
+		/// <param name="changeType">The type of change that occured.</param>
+		internal CollectionEventArgs(string source, string id, string collection, string type, EventType changeType)
+		{
+			this.source = source;
+			this.id = id;
+			this.collection = collection;
+			this.type = type;
+			this.changeType = changeType;
+		}
+
+		/// <summary>
+		/// Gets the string that represents the source of the event.
+		/// </summary>
+		public string Source
+		{
+			get {return Source;}
+		}
+
+		/// <summary>
+		/// Gets the ID of the affected Node/Collection.
+		/// </summary>
+		public string ID
+		{
+			get {return id;}
+		}
+		
+		/// <summary>
+		/// Gets the containing collection ID.
+		/// </summary>
+		public string Collection
+		{
+			get {return collection;}
+		}
+
+		/// <summary>
+		/// Gets the Type of the affected Node.
+		/// </summary>
+		public string Type
+		{
+			get {return type;}
+		}
+
+		public EventType ChangeType
+		{
+			get {return changeType;}
+			set {changeType = value;}
+		}
+	}
+}
