de2482b72ece9d6afad04aa71d8b2033af9dc6ce
(no author) <(no author)@db2431b9-df06-0410-971c-a38ea9a75485>
2004-03-13 01:10:45 +0000

    This commit was manufactured by cvs2svn to create branch 'NEWAPI-20040315'.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/NEWAPI-20040315@461 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/Sync/db.bat
+++
@@ -1,1 +0,0 @@
-"c:\Program Files\Microsoft.NET\SDK\v1.1\GuiDebug\DbgCLR.exe" SyncCmd.exe 
--- simias/src/Sync/t2client.bat
+++
@@ -1,9 +0,0 @@
-rd /s /q tmpRemote
-mkdir tmpRemote
-
-\msys\bin\scp olds@mud.provo.novell.com:simias/src/Sync/tmpClientData/invitation tmpRemote
-
-SyncCmd.exe -s tmpRemote accept tmpRemote\invitation tmpRemote
-
-SyncCmd.exe -s tmpRemote sync tmpRemote\testFolder
-
