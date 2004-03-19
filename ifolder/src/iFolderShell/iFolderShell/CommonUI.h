af9b3101ed2672b19e539e329dd1304fb007fe84
bgetter <bgetter@db2431b9-df06-0410-971c-a38ea9a75485>
2004-03-19 19:04:50 +0000

    Updated/added header information.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@597 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/src/iFolderShell/iFolderShell/CommonUI.h
+++ ifolder/src/iFolderShell/iFolderShell/CommonUI.h
@@ -1,26 +1,26 @@
 /***********************************************************************
- *  CommonUI.h - Common user interface routines
- * 
+ *  $RCSfile$
+ *
  *  Copyright (C) 2004 Novell, Inc.
  *
- *  This library is free software; you can redistribute it and/or
+ *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public
  *  License as published by the Free Software Foundation; either
  *  version 2 of the License, or (at your option) any later version.
  *
- *  This library is distributed in the hope that it will be useful,
+ *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Library General Public License for more details.
+ *  General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public
- *  License along with this library; if not, write to the Free
+ *  License along with this program; if not, write to the Free
  *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  *  Author: Bruce Getter <bgetter@novell.com>
- * 
+ *
  ***********************************************************************/
-
+ 
 #ifndef STRICT
 #define STRICT
 #endif
