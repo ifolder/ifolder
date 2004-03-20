3e00404260a9ce81ad4cd0771013120284722796
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2004-03-20 05:02:47 +0000

    updated all of the headers in this namespace to include the original headers from the authors of the files (why did we change them?).  Anyway, they are all set back the way they should be now.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@606 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/Smtp/SmtpTests.cs
+++ simias/src/Smtp/SmtpTests.cs
@@ -1,3 +1,35 @@
+/***********************************************************************
+ *  $RCSfile$
+ *
+ *  Copyright (C) 2004 Novell, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Author: Calvin Gaisford <cgaisford@novell.com>
+ *
+ ***********************************************************************/
+
+// This entire namespace (Simias.Mail) was taken and modified 
+// from the mono project.  It was part of the System.Web.Mail namespace.
+// We needed a way to send mail on all of the platforms we supported
+// and found that the mono implementation of System.Web.Mail was much
+// more usable.  We changed the namespace and included the code here
+// so we could use it for the peer notification on all of the platforms
+// including .Net on windows.
+
+
 using System;
 using System.IO;
 using NUnit.Framework;
