f96ae5dfa7e6f4cb13337d2e8a83857bf93794da
rlyon <rlyon@db2431b9-df06-0410-971c-a38ea9a75485>
2004-04-22 22:21:32 +0000

    Merge
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/Iteration7@749 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/SyncCommon/Library/SyncStore.cs
+++ simias/src/SyncCommon/Library/SyncStore.cs
@@ -49,7 +49,111 @@ namespace Simias.Sync
 		/// <returns>The collection object.</returns>
 		public SyncCollection OpenCollection(string id)
 		{
-			return null;
+			return GetCollection(baseStore.GetCollectionById(id));
+		}
+
+		/// <summary>
+		/// Create a collection.
+		/// </summary>
+		/// <param name="name">The name for the collection.</param>
+		/// <param name="path">The root path for the collection.</param>
+		/// <returns>The collection object of the created collection.</returns>
+		public SyncCollection CreateCollection(string name, string path)
+		{
+			Collection c = baseStore.CreateCollection(name, new Uri(Path.GetFullPath(path)));
+
+			return GetCollection(c);
+		}
+		
+		/// <summary>
+		/// Create a collection.
+		/// </summary>
+		/// <param name="id">The id (guid) for the collection.</param>
+		/// <param name="name">The name for the collection.</param>
+		/// <param name="type">The type for the collection.</param>
+		/// <param name="path">The root path for the collection.</param>
+		/// <returns>The collection object of the created collection.</returns>
+		public SyncCollection CreateCollection(string id, string name, string type, string path)
+		{
+			Collection c = new Collection(baseStore, name, id, type, new Uri(Path.GetFullPath(path)));
+
+			return GetCollection(c);
+		}
+		
+		/// <summary>
+		/// Create a collection.
+		/// </summary>
+		/// <param name="id">The id (guid) for the collection.</param>
+		/// <param name="name">The name for the collection.</param>
+		/// <param name="type">The type for the collection.</param>
+		/// <param name="path">The root path for the collection.</param>
+		/// <param name="role">The sync role for the collection.</param>
+		/// <returns>The collection object of the created collection.</returns>
+		public SyncCollection CreateCollection(string id, string name, string type, string path, SyncCollectionRoles role)
+		{
+			Collection c = new Collection(baseStore, name, id, type, new Uri(Path.GetFullPath(path)));
+
+			SyncCollection sc = GetCollection(c);
+
+			sc.Role = role;
+
+			return sc;
+		}
+		
+		/// <summary>
+		/// Create a collection.
+		/// </summary>
+		/// <param name="id">The id (guid) for the collection.</param>
+		/// <param name="name">The name for the collection.</param>
+		/// <param name="type">The type for the collection.</param>
+		/// <param name="path">The root path for the collection.</param>
+		/// <param name="role">The sync role for the collection.</param>
+		/// <param name="host">The sync host for the collection.</param>
+		/// <param name="port">The sync port for the collection.</param>
+		/// <returns>The collection object of the created collection.</returns>
+		public SyncCollection CreateCollection(string id, string name, string type, string path, SyncCollectionRoles role,
+			Uri MasterUri)
+		{
+			Collection c = new Collection(baseStore, name, id, type, new Uri(Path.GetFullPath(path)));
+
+			SyncCollection sc = GetCollection(c);
+
+			sc.Role = role;
+			sc.MasterUri = MasterUri;
+
+			return sc;
+		}
+		
+		/// <summary>
+		/// Create a collection.
+		/// </summary>
+		/// <param name="invitation">An invitation object with the required information to create a collection.</param>
+		/// <returns>The collection object of the created collection.</returns>
+		public SyncCollection CreateCollection(Invitation invitation)
+		{
+			// add any secret to the current identity chain
+			if ((invitation.PublicKey != null) && (invitation.PublicKey.Length > 0))
+			{
+				Identity identity = baseStore.CurrentIdentity;
+				identity.CreateAlias(invitation.Domain, invitation.Identity, invitation.PublicKey);
+				identity.Commit();
+			}
+	
+			return CreateCollection(invitation.CollectionId, invitation.CollectionName,
+				invitation.CollectionType, Path.Combine(invitation.RootPath, invitation.CollectionName),
+				SyncCollectionRoles.Slave, invitation.MasterUri);
+		}
+		
+		private SyncCollection GetCollection(Collection c)
+		{
+			SyncCollection sc = null;
+
+			if (c != null)
+			{
+				sc = new SyncCollection(c);
+			}
+
+			return sc;
 		}
 
 		/// <summary>
