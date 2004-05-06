92c8b99b2ad500a9c8ef9c8f72cff4874dd8e7df
rlyon <rlyon@db2431b9-df06-0410-971c-a38ea9a75485>
2004-05-06 21:57:50 +0000

    Added an NUnit config.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@932 db2431b9-df06-0410-971c-a38ea9a75485

---
+++ simias/tools/NUnit/net/config.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+REM enable the VisualStudio command line tools
+CALL "%VS71COMNTOOLS%\vsvars32.bat" > NUL
+
+REM config NUnit
+gacutil /i ./bin/nunit.core.dll /f
+gacutil /i ./nunit.framework.dll /f
