681f25dbf23f949ea01fa91e8df614778f34aaaf
bgetter <bgetter@db2431b9-df06-0410-971c-a38ea9a75485>
2004-05-06 22:59:42 +0000

    iFolder help files.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk/ifolder@939 db2431b9-df06-0410-971c-a38ea9a75485

---
+++ help/en/ui/feedback.js
@@ -0,0 +1,14 @@
+function addtofav(linkUrl,linkTitle){
+   if (!document.all) {
+     top.location.replace(linkUrl);
+     alert('Please press CTRL-D to bookmark this page');     
+   }
+   else external.AddFavorite(linkUrl,linkTitle);
+   return false;
+}
+
+document.write('<a href="#" class="navigation" onClick="window.open(\'http://www.novell.com/inc/rater.jsp?url=\' + ');
+document.write('document.location.href,\'_blank\',\'height=165,width=225,menubar=no,status=no\');">');
+self.document.writeln('  <img src="../../../ui/images/body_feedback.gif" border=0 alt="Feedback"></a>');
+self.document.writeln('  <img src="../../../ui/images/virt_dot-line.gif" border="0" alt="">');
+self.document.writeln('  <a href="javascript:;" class="navigation" onClick="addtofav(window.location.href,document.title); return false"><img src="../../../ui/images/body_bookmark.gif" border=0 alt="Bookmark this page"></a>');
