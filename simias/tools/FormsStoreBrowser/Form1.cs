1622cc48cad53674ac742bb73c1843294fea8fba
ryoung65 <ryoung65@db2431b9-df06-0410-971c-a38ea9a75485>
2004-05-07 19:35:20 +0000

    Initial checkin.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@960 db2431b9-df06-0410-971c-a38ea9a75485

---
+++ simias/tools/FormsStoreBrowser/Form1.cs
@@ -0,0 +1,353 @@
+/***********************************************************************
+ *  $RCSfile$
+ *
+ *  Copyright (C) 2004 Novell, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Author: Russ Young
+ *
+ ***********************************************************************/
+
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.IO;
+using Simias;
+using Simias.Storage;
+
+
+namespace StoreBrowser
+{
+	/// <summary>
+	/// Summary description for Form1.
+	/// </summary>
+	public class Form1 : System.Windows.Forms.Form
+	{
+		string path = null;
+		IStoreBrowser browser = null;
+		private System.Windows.Forms.MainMenu mainMenu1;
+		private System.Windows.Forms.MenuItem menuItem1;
+		private System.Windows.Forms.MenuItem menuItem3;
+		private System.Windows.Forms.MenuItem menuItem4;
+		private System.Windows.Forms.MenuItem menuItem5;
+		private System.Windows.Forms.MenuItem menuItem6;
+		private System.Windows.Forms.TreeView tView;
+		private System.Windows.Forms.MenuItem MI_OpenStore;
+		private System.Windows.Forms.MenuItem menuItem7;
+		private System.Windows.Forms.MenuItem menuItem8;
+		private System.Windows.Forms.MenuItem MI_Open_Provider;
+		private System.Windows.Forms.Splitter splitter1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.ImageList imageList1;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader ColumnName;
+		private System.Windows.Forms.ColumnHeader Value;
+		private System.Windows.Forms.ColumnHeader Type;
+		private System.ComponentModel.IContainer components;
+
+		public Form1()
+		{
+			//
+			// Required for Windows Form Designer support
+			//
+			InitializeComponent();
+			this.listView1.Hide();
+			tView.ImageList = imageList1;
+			tView.Dock = DockStyle.Fill;
+		}
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if (components != null) 
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.Resources.ResourceManager resources = new System.Resources.ResourceManager(typeof(Form1));
+			this.mainMenu1 = new System.Windows.Forms.MainMenu();
+			this.menuItem1 = new System.Windows.Forms.MenuItem();
+			this.MI_OpenStore = new System.Windows.Forms.MenuItem();
+			this.MI_Open_Provider = new System.Windows.Forms.MenuItem();
+			this.menuItem7 = new System.Windows.Forms.MenuItem();
+			this.menuItem3 = new System.Windows.Forms.MenuItem();
+			this.menuItem4 = new System.Windows.Forms.MenuItem();
+			this.menuItem8 = new System.Windows.Forms.MenuItem();
+			this.menuItem5 = new System.Windows.Forms.MenuItem();
+			this.menuItem6 = new System.Windows.Forms.MenuItem();
+			this.tView = new System.Windows.Forms.TreeView();
+			this.splitter1 = new System.Windows.Forms.Splitter();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.ColumnName = new System.Windows.Forms.ColumnHeader();
+			this.Value = new System.Windows.Forms.ColumnHeader();
+			this.Type = new System.Windows.Forms.ColumnHeader();
+			this.SuspendLayout();
+			// 
+			// mainMenu1
+			// 
+			this.mainMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+																					  this.menuItem1});
+			// 
+			// menuItem1
+			// 
+			this.menuItem1.Index = 0;
+			this.menuItem1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[] {
+																					  this.MI_OpenStore,
+																					  this.MI_Open_Provider,
+																					  this.menuItem7,
+																					  this.menuItem3,
+																					  this.menuItem4,
+																					  this.menuItem8,
+																					  this.menuItem5,
+																					  this.menuItem6});
+			this.menuItem1.Text = "File";
+			// 
+			// MI_OpenStore
+			// 
+			this.MI_OpenStore.Index = 0;
+			this.MI_OpenStore.Shortcut = System.Windows.Forms.Shortcut.CtrlS;
+			this.MI_OpenStore.Text = "Open Store";
+			this.MI_OpenStore.Click += new System.EventHandler(this.MI_Open_Store_Click);
+			// 
+			// MI_Open_Provider
+			// 
+			this.MI_Open_Provider.Index = 1;
+			this.MI_Open_Provider.Shortcut = System.Windows.Forms.Shortcut.CtrlP;
+			this.MI_Open_Provider.Text = "Open Provider";
+			this.MI_Open_Provider.Click += new System.EventHandler(this.MI_Open_Provider_Click);
+			// 
+			// menuItem7
+			// 
+			this.menuItem7.Index = 2;
+			this.menuItem7.Text = "-";
+			// 
+			// menuItem3
+			// 
+			this.menuItem3.Index = 3;
+			this.menuItem3.Text = "Close";
+			// 
+			// menuItem4
+			// 
+			this.menuItem4.Index = 4;
+			this.menuItem4.Text = "Delete";
+			// 
+			// menuItem8
+			// 
+			this.menuItem8.Index = 5;
+			this.menuItem8.Text = "-";
+			// 
+			// menuItem5
+			// 
+			this.menuItem5.Index = 6;
+			this.menuItem5.Text = "Export";
+			// 
+			// menuItem6
+			// 
+			this.menuItem6.Index = 7;
+			this.menuItem6.Text = "Import";
+			// 
+			// tView
+			// 
+			this.tView.Dock = System.Windows.Forms.DockStyle.Left;
+			this.tView.ImageIndex = -1;
+			this.tView.Location = new System.Drawing.Point(0, 0);
+			this.tView.Name = "tView";
+			this.tView.SelectedImageIndex = -1;
+			this.tView.Size = new System.Drawing.Size(296, 574);
+			this.tView.TabIndex = 0;
+			this.tView.AfterCollapse += new System.Windows.Forms.TreeViewEventHandler(this.tView_AfterCollapse);
+			this.tView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tView_AfterSelect);
+			this.tView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.tView_BeforeExpand);
+			// 
+			// splitter1
+			// 
+			this.splitter1.Location = new System.Drawing.Point(296, 0);
+			this.splitter1.Name = "splitter1";
+			this.splitter1.Size = new System.Drawing.Size(3, 574);
+			this.splitter1.TabIndex = 2;
+			this.splitter1.TabStop = false;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.richTextBox1.Location = new System.Drawing.Point(299, 0);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(613, 574);
+			this.richTextBox1.TabIndex = 3;
+			this.richTextBox1.Text = "";
+			this.richTextBox1.Visible = false;
+			// 
+			// imageList1
+			// 
+			this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+			this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			// 
+			// listView1
+			// 
+			this.listView1.AllowColumnReorder = true;
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+																						this.ColumnName,
+																						this.Value,
+																						this.Type});
+			this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listView1.GridLines = true;
+			this.listView1.Location = new System.Drawing.Point(299, 0);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(613, 574);
+			this.listView1.TabIndex = 4;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			this.listView1.Resize += new System.EventHandler(this.listView1_Resize);
+			// 
+			// ColumnName
+			// 
+			this.ColumnName.Text = "Name";
+			this.ColumnName.Width = 40;
+			// 
+			// Value
+			// 
+			this.Value.Text = "Value";
+			this.Value.Width = 40;
+			// 
+			// Type
+			// 
+			this.Type.Text = "Type";
+			this.Type.Width = 40;
+			// 
+			// Form1
+			// 
+			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+			this.ClientSize = new System.Drawing.Size(912, 574);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.splitter1);
+			this.Controls.Add(this.tView);
+			this.Menu = this.mainMenu1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main() 
+		{
+			Application.Run(new Form1());
+		}
+
+		private void MI_Open_Store_Click(object sender, System.EventArgs e)
+		{
+			tView.Nodes.Clear();
+			richTextBox1.Clear();
+			FolderBrowserDialog fb = new FolderBrowserDialog();
+			if (path == null)
+				path = new Configuration().StorePath;
+			fb.SelectedPath = path;
+			if (fb.ShowDialog() == DialogResult.OK)
+			{
+                path = fb.SelectedPath;
+				if (Path.GetFileName(path) == ".simias")
+				{
+					browser = new NodeBrowser(tView, listView1, path);
+					browser.Show();
+				}
+			}
+		}
+
+		private void MI_Open_Provider_Click(object sender, System.EventArgs e)
+		{
+			tView.Nodes.Clear();
+			richTextBox1.Clear();
+			FolderBrowserDialog fb = new FolderBrowserDialog();
+			if (path == null)
+				path = new Configuration().StorePath;
+			fb.SelectedPath = path;
+			if (fb.ShowDialog() == DialogResult.OK)
+			{
+				path = fb.SelectedPath;
+				if (Path.GetFileName(path) == ".simias")
+				{
+					browser = new ProviderBrowser(tView, richTextBox1, path);
+					browser.Show();
+				}
+			}
+		}
+
+		private void tView_AfterSelect(object sender, System.Windows.Forms.TreeViewEventArgs e)
+		{
+			browser.ShowNode(e.Node);
+		}
+
+		private void tView_BeforeExpand(object sender, System.Windows.Forms.TreeViewCancelEventArgs e)
+		{
+			//if (e.Node.Tag != null)
+			{
+				e.Node.Nodes.Clear();
+				browser.AddChildren(e.Node);	
+			}
+		}
+
+		private void tView_AfterCollapse(object sender, System.Windows.Forms.TreeViewEventArgs e)
+		{
+			if (e.Node.Tag != null)
+			{
+				e.Node.Nodes.Clear();
+				e.Node.Nodes.Add("Temp");
+			}
+			else
+			{
+				browser.Show();
+			}
+		}
+
+		private void listView1_Resize(object sender, System.EventArgs e)
+		{
+			int cwidth = listView1.Width / 3;
+			listView1.Columns[0].Width = listView1.Columns[1].Width = listView1.Columns[2].Width = cwidth;
+		}
+	}
+
+	public interface IStoreBrowser : IDisposable
+	{
+		void Show();
+		void ShowNode(TreeNode node);
+		void AddChildren(TreeNode tNode);
+	}
+}
---
+++ simias/tools/FormsStoreBrowser/NodeBrowser.cs
@@ -0,0 +1,212 @@
+/***********************************************************************
+ *  $RCSfile$
+ *
+ *  Copyright (C) 2004 Novell, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Author: Russ Young
+ *
+ ***********************************************************************/
+
+using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.IO;
+using Simias;
+using Simias.Storage;
+
+
+namespace StoreBrowser
+{
+	/// <summary>
+	/// Summary description for NodeBrowser.
+	/// </summary>
+	public class NodeBrowser : IStoreBrowser
+	{
+		Store store;
+		TreeView tView;
+		RichTextBox rBox;
+		ListView lView;
+		bool alreadyDisposed;
+
+		public NodeBrowser(TreeView view, ListView lView, string dbPath)
+		{
+			tView = view;
+			this.lView = lView;
+			lView.BringToFront();
+			lView.Show();
+			store = new Store(new Configuration(Path.GetDirectoryName(dbPath)));
+			tView.Dock = DockStyle.Left;
+			alreadyDisposed = true;
+		}
+
+		~NodeBrowser()
+		{
+			Dispose(true);
+		}
+
+		#region IStoreBrowser Members
+
+		public void Show()
+		{
+			tView.Nodes.Clear();
+			tView.BeginUpdate();
+			TreeNode storeNode = new TreeNode("Store");
+            tView.Nodes.Add(storeNode);
+			storeNode.Nodes.Add("temp");
+			tView.EndUpdate();
+		}
+
+		public void ShowNode(TreeNode tNode)
+		{
+			lView.Items.Clear();
+			if (tNode.Tag != null)
+			{
+				Node cNode = (Node)tNode.Tag;
+				// Add the name;
+				ListViewItem item = new ListViewItem("Name");
+				item.BackColor = Color.LightBlue;
+				item.SubItems.Add(cNode.Name);
+				item.SubItems.Add(Syntax.String.ToString());
+				lView.Items.Add(item);
+
+				// Add the ID.
+				item = new ListViewItem("ID");
+				item.BackColor = Color.White;
+				item.SubItems.Add(cNode.ID);
+				item.SubItems.Add(Syntax.String.ToString());
+				lView.Items.Add(item);
+
+				// Add the type.
+				item = new ListViewItem("Type");
+				item.BackColor = Color.LightBlue;
+				item.SubItems.Add(cNode.Type);
+				item.SubItems.Add(Syntax.String.ToString());
+				lView.Items.Add(item);
+
+				Color c = Color.LightBlue;
+				foreach (Property p in cNode.Properties)
+				{
+					c = c == Color.LightBlue ? Color.White : Color.LightBlue;
+					item = new ListViewItem(p.Name);
+					item.BackColor = c;
+					item.SubItems.Add(p.Value.ToString());
+					item.SubItems.Add(p.Type.ToString());
+					lView.Items.Add(item);
+				}
+			}
+			
+		}
+
+		#endregion
+
+		private void AddProperties(TreeNode tNode, Node node)
+		{	
+			tNode.Nodes.Add(new TreeNode("ID : " + node.ID, 1, 1));
+			tNode.Nodes.Add(new TreeNode("Type : " + node.Type, 1, 1));
+			Color c = Color.LightBlue;
+			foreach (Property p in node.Properties)
+			{
+				if (p.Type == Syntax.Relationship)
+				{
+				}
+				else
+				{
+					TreeNode pNode = new TreeNode(string.Format("{0,-20} : {1,40}", p.Name, p.Value.ToString()), 1, 1);
+					c = pNode.BackColor = (c == Color.LightBlue) ? Color.White : Color.LightBlue;
+					pNode.Nodes.Add(new TreeNode("Value: " + p.Value.ToString(), 1, 1));
+					pNode.Nodes.Add(new TreeNode("Type : " + p.Type.ToString(), 1, 1));
+					pNode.Nodes.Add(new TreeNode("Flags: " + p.Flags.ToString(), 1, 1));
+					tNode.Nodes.Add(pNode);
+				}
+			}
+		}
+
+		private void addCollections(TreeNode tNode)
+		{
+			tView.BeginUpdate();
+			foreach(ShallowNode sn in store)
+			{
+				Collection col = new Collection(store, sn);
+				TreeNode colNode = new TreeNode(col.Name);
+				tNode.Nodes.Add(colNode);
+				colNode.Tag = col;
+				colNode.Nodes.Add("temp");
+				//AddChildren(colNode, col);
+			}
+			tView.EndUpdate();
+		}
+
+		public void AddChildren(TreeNode tNode)
+		{
+			if (tNode.Tag == null)
+			{
+				addCollections(tNode);
+			}
+			else
+			{
+				Node node = (Node)tNode.Tag;
+				switch (node.Type)
+				{
+					case "Collection":
+						//AddProperties(tNode, node);
+						foreach (ShallowNode sn in (Collection)node)
+						{
+							if (sn.ID != node.ID)
+							{
+								Node n = new Node((Collection)node, sn);
+								TreeNode nNode = new TreeNode(n.Name);
+								nNode.Tag = n;
+								tNode.Nodes.Add(nNode);
+								nNode.Nodes.Add("temp");
+							}
+							//AddChildren(nNode);
+						}
+						break;
+					default:
+						//AddProperties(tNode, node);
+						break;
+				}
+			}
+		}
+
+		private void Dispose(bool inFinalize)
+		{
+			if (!alreadyDisposed)
+			{
+				if (!inFinalize)
+				{
+					GC.SuppressFinalize(this);
+				}
+				store.Dispose();
+				alreadyDisposed = true;
+			}
+		}
+        
+		#region IDisposable Members
+
+		public void Dispose()
+		{
+			Dispose(false);
+		}
+
+		#endregion
+	}
+
+}
