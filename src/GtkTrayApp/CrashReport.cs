d8e20ef0056a8039c73267cc01218add8c8b1990
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2004-05-12 22:51:44 +0000

    I added in a CrashReport Dialog that will be shown when the app dies. GtkTRayApp now catches any exceptions that might be thrown and shuts everything down the say it should
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk/ifolder@976 db2431b9-df06-0410-971c-a38ea9a75485

---
+++ src/GtkTrayApp/CrashReport.cs
@@ -0,0 +1,83 @@
+/***********************************************************************
+ *  $RCSfile$
+ *
+ *  Copyright (C) 2004 Novell, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Author: Calvin Gaisford <cgaisford@novell.com>
+ *
+ ***********************************************************************/
+
+using System;
+using System.Drawing;
+
+using Gtk;
+using Gdk;
+using Gnome;
+using Glade;
+using GtkSharp;
+using GLib;
+
+namespace Novell.iFolder
+{
+	public class CrashReport
+	{
+		[Glade.Widget] internal Gtk.TextView CrashTextView;
+		[Glade.Widget] internal Gtk.Button OKButton;
+
+		Gtk.Dialog 		crDlg = null;
+
+		public string CrashText
+		{
+			set
+			{
+				if(CrashTextView != null)
+					CrashTextView.Buffer.Text = value;
+			}
+		}
+
+		public Gtk.Window TransientFor
+		{
+			set
+			{
+				if(crDlg != null)
+					crDlg.TransientFor = value;
+			}
+		}
+
+		public CrashReport() 
+		{
+			Glade.XML gxml = new Glade.XML ("crash-report.glade",
+					"CrashDialog", null);
+			gxml.Autoconnect (this);
+
+			crDlg = (Gtk.Dialog) gxml.GetWidget("CrashDialog");
+		}
+
+		public int Run()
+		{
+			int rc = 0;
+			if(crDlg != null)
+			{
+				rc = crDlg.Run();
+				crDlg.Hide();
+				crDlg.Destroy();
+				crDlg = null;
+			}
+			return rc;
+		}
+	}
+}
