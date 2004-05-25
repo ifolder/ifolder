776f227927019781d2eab41831b0eeaa1939ae8a
olds <olds@db2431b9-df06-0410-971c-a38ea9a75485>
2004-05-25 16:15:53 +0000

    crap.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@1102 db2431b9-df06-0410-971c-a38ea9a75485

---
+++ simias/src/Sync/SyncSize.cs
@@ -0,0 +1,80 @@
+/***********************************************************************
+ *  $RCSfile$
+ *
+ *  Copyright (C) 2004 Novell, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Author: Dale Olds <olds@novell.com>
+ *
+ ***********************************************************************/
+using System;
+//using System.Collections;
+using System.IO;
+//using System.Diagnostics;
+using System.Runtime.Serialization.Formatters.Binary;
+
+using Simias.Storage;
+using Simias;
+
+namespace Simias.Sync
+{
+
+//---------------------------------------------------------------------------
+/// <summary>
+/// class to approximate amount of data that is out of sync with master
+/// Note that this is worst-case of data that may need to be sent from
+/// this collection to the master. It does not include data that may need
+/// to be retrieved from the master. It also does not account for
+/// delta-sync algorithms that may reduce what needs to be sent
+/// </summary>
+internal class SyncSize
+{
+	//--------------------------------------------------------------------
+	public static void CalculateSendSize(Collection collection, out uint fileCount, out ulong maxBytesToSend)
+	{
+		maxBytesToSend = 0;
+		fileCount = 0;
+		Log.Spew("starting to calculate size to send to master for collection {1}", collection.Name);
+
+		// TODO: would be nice to have the database find all nodes for which MasterIncarnation != LocalIncarnation
+		foreach (ShallowNode sn in collection)
+		{
+			Node node = new Node(collection, sn);
+			if (node.MasterIncarnation != node.LocalIncarnation)
+			{
+				long fileSize = 0;
+				if (collection.IsType(node, typeof(BaseFileNode).Name))
+				{
+					BaseFileNode bfn = (BaseFileNode)node;
+					FileInfo fi = new FileInfo(bfn.GetFullPath(collection));
+					fileSize = fi.Length;
+				}
+
+				// TODO: would be nice to have a faster to get serializable-size rather than actually serializing it.
+				MemoryStream ms = new MemoryStream();
+				BinaryFormatter bf = new BinaryFormatter();
+				bf.Serialize(ms, node);
+				Log.Spew("Adding node {0} to send size: fileSize = {1}, nodeSize = {2}", node.Name, fileSize, ms.Length);
+				maxBytesToSend += (ulong)fileSize + (ulong)ms.Length;
+				fileCount++;
+			}
+		}
+	}
+
+}
+
+//===========================================================================
+}
