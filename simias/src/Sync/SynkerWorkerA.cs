5f0ec1e51ec3c25812f7c6db1be9754e0c5b7df4
ryoung65 <ryoung65@db2431b9-df06-0410-971c-a38ea9a75485>
2004-07-07 20:52:36 +0000

    Added a klude to allow access control until we get authentication.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@1536 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/Sync/SynkerWorkerA.cs
+++ simias/src/Sync/SynkerWorkerA.cs
@@ -42,6 +42,7 @@ public class SynkerWorkerA: SyncCollectionWorker
 	//TODO: why is the base master and slave not protected instead of private?
 	SynkerServiceA ss;
 	SyncCollection collection;
+	Member		   me;
 
 	Hashtable	largeFromServer, smallFromServer, dirsFromServer;
 	Hashtable	largeToServer, smallToServer, dirsToServer;
@@ -71,6 +72,9 @@ public class SynkerWorkerA: SyncCollectionWorker
 		Log.Spew("-------- starting sync pass for collection {0}", collection.Name);
 		try
 		{
+			// Save off the member we are running as.
+			me = collection.GetCurrentMember();
+			// Now impersonate the collection so that we can import the nodes from the server.
 			collection.Impersonate(new Member("Root", collection.ID, Access.Rights.Admin));
 			try
 			{
@@ -173,7 +177,7 @@ public class SynkerWorkerA: SyncCollectionWorker
 	/// </summary>
 	void DoOneSyncPass()
 	{
-		Access.Rights rights = ss.Start(collection.GetCurrentMember().UserID);
+		Access.Rights rights = ss.Start(me.UserID);
 		if (rights == Access.Rights.Deny)
 		{
 			Log.Error("Sync with collection {0} denied", collection.Name);
