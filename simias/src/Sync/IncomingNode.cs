fab785e57e58bbc57d5aa4ae1d3c5840a5431150
ryoung65 <ryoung65@db2431b9-df06-0410-971c-a38ea9a75485>
2004-07-08 19:06:48 +0000

    Made fix to sync store managed files.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@1569 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/Sync/IncomingNode.cs
+++ simias/src/Sync/IncomingNode.cs
@@ -247,7 +247,7 @@ internal class IncomingNode
 		BaseFileNode bfn = SyncOps.CastToBaseFileNode(collection, node);
 		if (bfn == null)
 		{
-			Log.log.Debug("commiting nonFile, nonDir {0}", node.GetFileName());
+			Log.log.Debug("commiting nonFile, nonDir {0}", node.Name);
 			Log.Assert(forkList == null);
 			ClearTemp();
 			return status;
