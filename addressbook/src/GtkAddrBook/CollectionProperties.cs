75819445740d4e2541a0a2cbd3c9fe96826b9fe7
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2004-07-21 22:25:12 +0000

    updated to add the ColPropViewer.exe for the Virtual Office Team
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@1730 db2431b9-df06-0410-971c-a38ea9a75485

--- addressbook/src/GtkAddrBook/CollectionProperties.cs
+++ addressbook/src/GtkAddrBook/CollectionProperties.cs
@@ -49,6 +49,9 @@ namespace Novell.AddressBook.UI.gtk
 
 		private int activeTag = 0;
 		private Collection collection;
+		private bool runCalled = false;
+
+		public event EventHandler Closed;
 
 		public Gtk.Window TransientFor
 		{
@@ -82,6 +85,7 @@ namespace Novell.AddressBook.UI.gtk
 
 		public CollectionProperties() 
 		{
+			InitGlade();
 		}
 
 		public void InitGlade()
@@ -91,16 +95,14 @@ namespace Novell.AddressBook.UI.gtk
 				"PropDialog", 
 				null);
 			gxml.Autoconnect (this);
-
-			InitDialog();
 		}
 
 		private void InitDialog()
 		{
-			Console.WriteLine("Initializing Dialog");
+			//Console.WriteLine("Initializing Dialog");
 			if(collection != null)
 			{
-				Console.WriteLine("Have a valid Collection");
+				//Console.WriteLine("Have a valid Collection");
 
 				propNoteBook = new Gtk.Notebook();
 				sharingPage = new ContactCollectionSharingPage();
@@ -111,17 +113,17 @@ namespace Novell.AddressBook.UI.gtk
 				generalPage.Collection = collection;
 				propPage.Collection = collection;
 
-				Console.WriteLine("Adding General Page");
+				//Console.WriteLine("Adding General Page");
 				propNoteBook.AppendPage(generalPage.MainWidget, 
 						new Label("General"));
-				Console.WriteLine("Adding Sharing Page");
+				//Console.WriteLine("Adding Sharing Page");
 				propNoteBook.AppendPage(sharingPage.MainWidget, 
 						new Label("Sharing"));
-				Console.WriteLine("Adding Prop Page");
+				//Console.WriteLine("Adding Prop Page");
 				propNoteBook.AppendPage(propPage.MainWidget, 
 						new Label("All Properties"));
 
-				Console.WriteLine("Connecting all of the stuff");
+				//Console.WriteLine("Connecting all of the stuff");
 
 				DialogVBox.PackStart(propNoteBook);
 				DialogVBox.ShowAll();
@@ -132,7 +134,7 @@ namespace Novell.AddressBook.UI.gtk
 		{
 			int rc = 0;
 
-			InitGlade();
+			InitDialog();
 
 			if(PropDialog != null)
 			{
@@ -141,6 +143,7 @@ namespace Novell.AddressBook.UI.gtk
 
 				while(rc == 0)
 				{
+					runCalled = true;
 					rc = PropDialog.Run();
 					if(rc == -11) // help
 					{
@@ -160,11 +163,56 @@ namespace Novell.AddressBook.UI.gtk
 					}
 				}
 
-				PropDialog.Hide();
-				PropDialog.Destroy();
-				PropDialog = null;
+				on_close(null, null);
 			}
 			return rc;
+		}
+
+
+		public void Show()
+		{
+			int rc = 0;
+
+			InitDialog();
+
+			if(PropDialog != null)
+			{
+				if(propNoteBook.NPages >= activeTag)
+					propNoteBook.CurrentPage = activeTag;
+
+				PropDialog.Show();
+			}
+		}
+
+
+		public void on_close(object o, EventArgs args)
+		{
+			//Console.WriteLine("on_close is being called");
+			PropDialog.Hide();
+			PropDialog.Destroy();
+			PropDialog = null;
+			if(Closed != null)
+			{
+				EventArgs e = new EventArgs();
+				Closed(this, e);
+			}
+		}
+
+		public void on_delete_event(object o, DeleteEventArgs args) 
+		{
+			args.RetVal = true;
+			on_close(o, args);
+		}
+
+		public void on_close_clicked(object o, EventArgs args) 
+		{
+			// if Run was called (not Show), we don't want to call on_close
+			// because the close button already sends a signal
+			// when it is clicked and we respond to it
+			if(!runCalled)
+			{
+				on_close(o, args);
+			}
 		}
 	}
 }
