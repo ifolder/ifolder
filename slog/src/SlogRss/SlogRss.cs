65d1923e9028d34591868163ac79f9c0e1e969d5
bradya <bradya@db2431b9-df06-0410-971c-a38ea9a75485>
2004-07-23 16:11:44 +0000

    added support to stream all slogs in a store
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@1741 db2431b9-df06-0410-971c-a38ea9a75485

--- slog/src/SlogRss/SlogRss.cs
+++ slog/src/SlogRss/SlogRss.cs
@@ -1,6 +1,29 @@
-//<%@ WebService Language="c#" %>
+/************************************************************************
+ *  $RCSfile$
+ *
+ *  Copyright (C) 2004 Novell, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Author: Brady Anderson <banderso@novell.com>
+ *
+ ***********************************************************************/
 
+using System;
 using System.Web;
+using Novell.Collaboration;
 
 namespace Novell.Collaboration
 {
@@ -8,29 +31,63 @@ namespace Novell.Collaboration
     {
         public void ProcessRequest(HttpContext context)
         {
-            //context.Response.Write("Hello World!");
-            
-            // Kewl now we can slog away
-            context.Response.Write("<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>");
-			context.Response.Write("<rss version=\"2.0\">");
-			context.Response.Write("</rss>");
+        	Console.WriteLine("ProcessRequest entered");
+        	
+        	bool	first = true;
+        	
+			SlogManager slogManager = SlogManager.Connect();
+ 			foreach(Slog slog in slogManager)
+			{
+				if (first == true)
+				{	
+		            context.Response.ContentType = "text/xml";
+		            context.Response.Write("<?xml version=\"1.0\" encoding=\"iso-8859-1\"?>");
+					context.Response.Write("<rss version=\"2.0\">");
+					first = false;
+				}
+				
+				context.Response.Write("<channel>");
+				
+				
+				context.Response.Write("<title>");
+				context.Response.Write(slog.Name);
+				context.Response.Write("</title>");
+
+								
+				context.Response.Write("<description>");
+				context.Response.Write("Ah, we need to add a description property to a slog");
+				context.Response.Write("</description>");
+				
+				foreach(SlogEntry slogEntry in slog)
+				{
+					context.Response.Write("<item>");
+					
+					context.Response.Write("<title>");
+					context.Response.Write(slogEntry.Title);
+					context.Response.Write("</title>");
+					
+					context.Response.Write("<pubDate>");
+					context.Response.Write(slogEntry.Name);
+					context.Response.Write("</pubDate>");
+					
+					context.Response.Write("<description>");
+					context.Response.Write(slogEntry.Description);
+					context.Response.Write("</description>");
 			
-			context.Response.Write("<channel>");
-			context.Response.Write("<title>iFolderlogue</title>");
-			context.Response.Write("<description>The voices of iFolder development</description>");
-			context.Response.Write("<item>");
-			context.Response.Write("<title>");
-			context.Response.Write("Brady Anderson: Simias III");
-			context.Response.Write("</title>");
-			
-			context.Response.Write("<description>");
-			context.Response.Write("Today Mike Lasky released version 3 of the Simias architecture document");
-			context.Response.Write("</description>");
-			
-			context.Response.Write("</item>");
-			 			
-			context.Response.Write("</channel>");
-			context.Response.Write("</rss>");
+					context.Response.Write("</item>");
+				}
+				
+				context.Response.Write("</channel>");
+			}
+	
+			if (first == false)
+			{
+				context.Response.Write("</rss>");
+			}
+			else
+			{
+				// FIXME - set an error
+			}
         }
 
         public bool IsReusable
