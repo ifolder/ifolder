603eb4bed9acb759851c2e2f281bf3114faf5d89
pthomas707 <pthomas707@db2431b9-df06-0410-971c-a38ea9a75485>
2004-10-04 17:50:09 +0000

    Added "mDnsService" as description for bugzilla bug 66371.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@2223 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/mDnsService/AssemblyInfo.cs
+++ simias/src/mDnsService/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("")]
+[assembly: AssemblyTitle("mDnsService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyProduct("mDnsService")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
