1b3958455af4448b543d51d7fbbb8495963dec40
pthomas707 <pthomas707@db2431b9-df06-0410-971c-a38ea9a75485>
2004-10-05 22:36:26 +0000

    Added "Simias" as description for bugzilla bug 67640.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@2243 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/SimiasApp/AssemblyInfo.cs
+++ simias/src/SimiasApp/AssemblyInfo.cs
@@ -15,11 +15,11 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("")]
+[assembly: AssemblyTitle("Simias")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyProduct("Simias")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
