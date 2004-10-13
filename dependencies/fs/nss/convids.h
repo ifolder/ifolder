0c894c5d5abe52e392ab0b4527c3d03a9954686e
(no author) <(no author)@db2431b9-df06-0410-971c-a38ea9a75485>
2004-10-13 20:32:37 +0000

    This commit was manufactured by cvs2svn to create branch 'win_i18n'.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/win_i18n@2339 db2431b9-df06-0410-971c-a38ea9a75485

--- dependencies/fs/nss/convids.h
+++
@@ -1,8 +0,0 @@
-#ifndef _CONVIDS_H_
-#define _CONVIDS_H_
-
-/* Function prototypes */
-STATUS FS_ConvertGuidToID(NDSid_t *guid, LONG *idp);
-STATUS FS_ConvertIDToGuid(LONG id, NDSid_t *guid);
-STATUS CFS_ConvertGuidToID( LONG *guid, LONG id);
-#endif /* _CONVIDS_H_ */
