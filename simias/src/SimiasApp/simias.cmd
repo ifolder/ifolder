714341885a22d22496f62a5bbfb351ed9fbf9e4f
mlasky <mlasky@db2431b9-df06-0410-971c-a38ea9a75485>
2004-11-09 17:25:51 +0000

    1. Added code to the test scripts to automatically copy the inspection.wsil file to the right place and add the addition virtual directory.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@2603 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/SimiasApp/simias.cmd
+++ simias/src/SimiasApp/simias.cmd
@@ -1,1 +1,2 @@
-@web\bin\SimiasApp.exe --applications /simias10:web --port 8086
+@if not exist web\inspection.wsil copy etc\inspection.wsil web\inspection.wsil >nul
+@web\bin\SimiasApp.exe --applications /:web,/simias10:web --port 8086
