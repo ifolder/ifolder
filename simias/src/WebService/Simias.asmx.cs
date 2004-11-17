d0238198fd6489d6bf12071ea99e5cd667a9f232
bradya <bradya@db2431b9-df06-0410-971c-a38ea9a75485>
2004-11-17 23:53:50 +0000

    added methods to set credentials for the simias process, also added a method to get local domain information
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@2728 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/WebService/Simias.asmx.cs
+++ simias/src/WebService/Simias.asmx.cs
@@ -25,16 +25,22 @@ using System.Collections;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Services;
 using System.Web.Services.Protocols;
 using System.IO;
-using Simias;
-//using Simias.Storage;
-//using Simias.Sync;
-//using Simias.POBox;
 using System.Xml;
 using System.Xml.Serialization;
+
+using Novell.Security.ClientPasswordManager;
+
+using Simias;
+using Simias.Client;
+using Simias.Storage;
+using Simias.Sync;
+//using Simias.POBox;
 
 namespace Simias.Web
 {
@@ -48,6 +54,7 @@ namespace Simias.Web
 	Description="Web Service providing access to Simias")]
 	public class SimiasService : WebService
 	{
+		private static readonly ISimiasLog log = SimiasLogManager.GetLogger(typeof(SimiasService));
 
 		/// <summary>
 		/// Creates the SimiasService and sets up logging
@@ -56,9 +63,6 @@ namespace Simias.Web
 		{
 		}
 	
-
-
-
 		/// <summary>
 		/// WebMethod that returns the Simias information
 		/// </summary>
@@ -66,10 +70,264 @@ namespace Simias.Web
 		/// string with Simias information
 		/// </returns>
 		[WebMethod(Description="GetSimiasInformation")]
-		[SoapRpcMethod]
+		[SoapDocumentMethod]
 		public string GetSimiasInformation()
 		{
 			return "TODO: Implement the Simias Web Service";
 		}
+
+
+		/// <summary>
+		/// WebMethod to get information about a specified domain 
+		/// </summary>
+		/// <returns>
+		/// 0 success, !0 failed
+		/// </returns>
+		[WebMethod(Description="GetDomainInformation")]
+		[SoapDocumentMethod]
+		public
+		DomainInformation
+		GetDomainInformation(string domainID)
+		{
+			DomainInformation cDomainInfo = null;
+			try
+			{
+				Store store = Store.GetStore();
+
+				Simias.Storage.Domain cDomain = store.GetDomain(domainID);
+				Roster cRoster = cDomain.GetRoster(store);
+				Member cMember = cRoster.GetCurrentMember();
+
+				cDomainInfo = new DomainInformation();
+
+				cDomainInfo.Type =
+					(domainID == Simias.Storage.Domain.WorkGroupDomainID)
+						? DomainType.Workgroup
+						: DomainType.Enterprise;
+
+				cDomainInfo.ID = domainID;
+				cDomainInfo.Name = cDomain.Name;
+				cDomainInfo.Description = cDomain.Description;
+				cDomainInfo.RosterID = cRoster.ID;
+				cDomainInfo.RosterName = cRoster.Name;
+				cDomainInfo.RosterID = cRoster.ID;
+				cDomainInfo.MemberID = cMember.UserID;
+				cDomainInfo.MemberName = cMember.Name;
+				cDomainInfo.RemoteUrl = 
+					cDomain.HostAddress.ToString() + "/DomainService.asmx";
+			}
+			catch(Exception e)
+			{
+				log.Debug(e.Message);
+				log.Debug(e.StackTrace);
+				cDomainInfo = null;
+			}
+
+			return(cDomainInfo);
+		}
+
+
+		/// <summary>
+		/// WebMethod that sets credential information for a 
+		/// member to a domain
+		/// </summary>
+		/// <returns>
+		/// 0 success, !0 failed
+		/// </returns>
+		[WebMethod(Description="SetDomainCredentials")]
+		[SoapDocumentMethod]
+		public
+		int SetDomainCredentials(
+				string			domainID,
+				string			memberID,
+				string			password)
+		{
+			int status = -1;
+			try
+			{
+				Store store = Store.GetStore();
+
+				// Make sure the Domain ID is not workgroup
+				if (domainID != Simias.Storage.Domain.WorkGroupDomainID)
+				{
+					Simias.Storage.Domain cDomain = store.GetDomain(domainID);
+					Roster cRoster = cDomain.GetRoster(store);
+					Member cMember = cRoster.GetMemberByID(memberID);
+
+					NetCredential cCreds = 
+						new NetCredential("iFolder", domainID, true, cMember.Name, password);
+
+					if (cCreds != null)
+					{
+						status = 0;
+					}
+				}
+			}
+			catch(Exception e)
+			{
+				log.Debug(e.Message);
+				log.Debug(e.StackTrace);
+			}
+
+			return(status);
+		}
+
+
+		/// <summary>
+		/// WebMethod that checks to see if a full set of credentials
+		/// has been set on a domain for a specified user
+		/// </summary>
+		/// <returns>
+		/// true - valid credentials for member on the domain, false
+		/// </returns>
+		[WebMethod(Description="ValidCredentials")]
+		[SoapDocumentMethod]
+		public
+		bool ValidCredentials(string domainID, string memberID)
+		{
+			bool status = false;
+			try
+			{
+				Store store = Store.GetStore();
+
+				// Make sure the Domain ID is not workgroup
+				if (domainID != Simias.Storage.Domain.WorkGroupDomainID)
+				{
+					// roster
+					Simias.Storage.Domain cDomain = store.GetDomain(domainID);
+					Roster roster = cDomain.GetRoster(store);
+
+					// find user
+					Member cMember = roster.GetMemberByID(memberID);
+
+					NetCredential cCreds = 
+						new NetCredential("iFolder", domainID, true, cMember.Name, null);
+
+					UriBuilder cUri = 
+						new UriBuilder(
+							this.Context.Request.Url.Scheme,
+							this.Context.Request.Url.Host,
+							this.Context.Request.Url.Port,
+							this.Context.Request.ApplicationPath.TrimStart( new char[] {'/'} ));
+
+					NetworkCredential realCreds = cCreds.GetCredential(cUri.Uri, "BASIC");
+					if (realCreds != null)
+					{
+						status = true;
+					}
+				}
+			}
+			catch(Exception e)
+			{
+				log.Debug(e.Message);
+				log.Debug(e.StackTrace);
+			}
+
+			return(status);
+		}
+	}
+
+	/// <summary>
+	/// Type of Domain (Enterprise/Workgroup)
+	/// </summary>
+	[Serializable]
+	public enum DomainType
+	{
+		/// <summary>
+		/// Workgroup domain
+		/// </summary>
+		Workgroup,
+
+		/// <summary>
+		/// Enterprise domain
+		/// </summary>
+		Enterprise,
+	};
+
+	/// <summary>
+	/// Domain information
+	/// </summary>
+	[Serializable]
+	public class DomainInformation
+	{
+		/// <summary>
+		/// Domain Type
+		/// </summary>
+		public DomainType Type;
+
+		/// <summary>
+		/// Domain Name
+		/// </summary>
+		public string Name;
+
+		/// <summary>
+		/// Domain Description
+		/// </summary>
+		public string Description;
+
+		/// <summary>
+		/// Domain ID
+		/// </summary>
+		public string ID;
+
+		/// <summary>
+		/// Domain Roster (Member List) Collection ID
+		/// </summary>
+		public string RosterID;
+
+		/// <summary>
+		/// Domain Roster (Member List) Collection Name
+		/// </summary>
+		public string RosterName;
+
+		/// <summary>
+		/// The unique member/user ID.
+		/// </summary>
+		public string MemberID;
+
+		/// <summary>
+		/// The name of the member object
+		/// </summary>
+		public string MemberName;
+
+		/// <summary>
+		/// Url to the remote domain service
+		/// </summary>
+		public string RemoteUrl;
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public DomainInformation()
+		{
+		}
+
+		/// <summary>
+		/// Create a string representation
+		/// </summary>
+		/// <returns>A string representation</returns>
+		public override string ToString()
+		{
+			StringBuilder builder = new StringBuilder();
+			
+			string newLine = Environment.NewLine;
+
+			builder.AppendFormat("Domain Information{0}", newLine);
+			builder.AppendFormat("  ID               : {0}{1}", this.ID, newLine);
+			builder.AppendFormat("  Type             : {0}{1}", this.Type.ToString(), newLine);
+			builder.AppendFormat("  Name             : {0}{1}", this.Name, newLine);
+			builder.AppendFormat("  Description      : {0}{1}", this.Description, newLine);
+			builder.AppendFormat("  Roster ID        : {0}{1}", this.RosterID, newLine);
+			builder.AppendFormat("  Roster Name      : {0}{1}", this.RosterName, newLine);
+			builder.AppendFormat("  Member Node ID   : {0}{1}", this.MemberID, newLine);
+			builder.AppendFormat("  Member Node Name : {0}{1}", this.MemberName, newLine);
+			builder.AppendFormat("  Remote Url       : {0}{1}", this.RemoteUrl, newLine);
+
+			return builder.ToString();
+		}
 	}
 }
+
+
+
+
