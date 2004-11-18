29762aec20bb9ed6f7086932998ae78c81dba345
bradya <bradya@db2431b9-df06-0410-971c-a38ea9a75485>
2004-11-18 00:17:34 +0000

    Initial checkin - new threadservice that generates events for remote domain that have not been authenticated against
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@2731 db2431b9-df06-0410-971c-a38ea9a75485

---
+++ simias/src/DomainWatcher/DomainWatcher.cs
@@ -0,0 +1,225 @@
+/***********************************************************************
+ *  $RCSfile$
+ *
+ *  Copyright (C) 2004 Novell, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public
+ *  License along with this program; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  Author: banderso@novell.com
+ *
+ ***********************************************************************/
+
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Net;
+using System.Runtime.Remoting;
+using System.Threading;
+using System.Web;
+
+using Simias;
+using Simias.Client;
+//using Simias.Client.Event;
+using Simias.Event;
+using Simias.Storage;
+using Simias.Sync;
+
+using Novell.Security.ClientPasswordManager;
+
+namespace Simias.DomainWatcher
+{
+	/// <summary>
+	/// PO Manager
+	/// </summary>
+	public class Manager : IDisposable
+	{
+		private static readonly ISimiasLog log = 
+			SimiasLogManager.GetLogger(typeof(Simias.DomainWatcher.Manager));
+
+		private bool			started = false;
+		private	bool			stop = false;
+		private Configuration	config;
+		private Thread			watcherThread = null;
+		private AutoResetEvent	stopEvent;
+		private Store			store;
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="config">Simias configuration</param>
+		public Manager(Configuration config)
+		{
+			this.config = config;
+			
+			// store
+			store = Store.GetStore();
+
+		}
+
+		/// <summary>
+		/// Start the Watcher.
+		/// </summary>
+		public void Start()
+		{
+			log.Info("DomainWatcher::Start called");
+
+			try
+			{
+				lock(this)
+				{
+					if (started == false)
+					{
+						this.watcherThread = new Thread(new ThreadStart(this.WatcherThread));
+						this.watcherThread.IsBackground = true;
+						this.watcherThread.Priority = ThreadPriority.BelowNormal;
+						this.stopEvent = new AutoResetEvent(false);
+
+						this.watcherThread.Start();
+					}
+				}
+			}
+			catch(Exception e)
+			{
+				log.Error(e, "Unable to start Domain Watcher thread.");
+				throw e;
+			}
+		}
+
+		/// <summary>
+		/// Stop the Domain Watcher Thread.
+		/// </summary>
+		public void Stop()
+		{
+			log.Info("DomainWatcher::Stop called");
+			try
+			{
+				lock(this)
+				{
+					// Set state and then signal the event
+					this.stop = true;
+					this.stopEvent.Set();
+					Thread.Sleep(32);
+					this.stopEvent.Close();
+					Thread.Sleep(0);
+				}
+			}
+			catch(Exception e)
+			{
+				log.Error(e, "Unable to stop Domain Watcher.");
+				throw e;
+			}
+		}
+
+		/// <summary>
+		/// Domain Watcher Thread.
+		/// </summary>
+		public void WatcherThread()
+		{
+			log.Info("DomainWatcher thread started");
+			int status;
+			this.started = true;
+			do 
+			{
+				//
+				// Cycle through the domains
+				// uh, right now there can only be one Enterprise Domain
+				//
+
+				try
+				{
+					Simias.Storage.Domain cDomain = store.GetDomain(store.DefaultDomain);
+					if (cDomain != null &&
+						cDomain.ID != Simias.Storage.Domain.WorkGroupDomainID) 
+					{
+						Roster cRoster = cDomain.GetRoster(store);
+						Member cMember = cRoster.GetCurrentMember();
+
+						// Can we talk to the domain?
+						// Check to see if a full set of credentials exist
+						// for this domain
+
+						NetCredential cCreds = 
+							new NetCredential(
+								"iFolder", 
+								cDomain.ID, 
+								true, 
+								cMember.Name, 
+								null);
+
+						Uri cUri = new Uri(cDomain.HostAddress.ToString());
+						if (cCreds.GetCredential(cUri, "BASIC") == null)
+						{
+							// Create the domain service web client object.
+							DomainService domainSvc = new DomainService();
+							domainSvc.Url = 
+								cDomain.HostAddress.ToString() + "/DomainService.asmx";
+							
+							try
+							{
+								domainSvc.GetDomainInfo(cMember.ID);
+								status = 0;
+							}
+							catch(WebException webEx)
+							{
+								status = -1;
+								if (webEx.Status == System.Net.WebExceptionStatus.ProtocolError ||
+									webEx.Status == System.Net.WebExceptionStatus.TrustFailure )
+								{
+									status = 0;
+								}
+							}
+
+							if (status == 0)
+							{
+								EventPublisher cEvent = new EventPublisher();
+								Simias.Client.Event.NotifyEventArgs cArg =
+									new Simias.Client.Event.NotifyEventArgs(
+									"Domain-Up", 
+									cDomain.ID, 
+									System.DateTime.Now);
+
+								cEvent.RaiseEvent(cArg);
+							}
+
+							domainSvc = null;
+						}
+					}
+				}
+				catch(Exception e)
+				{
+					log.Debug("DomainWatcher::WatcherThread");
+					log.Debug(e.Message);
+					log.Debug(e.StackTrace);
+				}
+
+				stopEvent.WaitOne((30 * 1000), false);
+			} while(this.stop == false);
+
+			this.started = false;
+		}
+
+		#region IDisposable Members
+
+		/// <summary>
+		/// Dispose
+		/// </summary>
+		public void Dispose()
+		{
+			Stop();
+		}
+
+		#endregion
+	}
+}
