9ef5528de3e6321bcc3b2b3657c4a1a12fb45465
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2005-01-26 15:19:21 +0000

    updated to resize graphics
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@3322 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/src/LinuxClient/library/iFolderLoginDialog.cs
+++ ifolder/src/LinuxClient/library/iFolderLoginDialog.cs
@@ -37,6 +37,9 @@ namespace Novell.iFolder
 		private string  DomainUserName;
 		private bool	FullDialog;
 
+		private Image				 iFolderBanner;
+		private Image				 iFolderScaledBanner;
+		private Gdk.Pixbuf			 ScaledPixbuf;
 
 		public string UserName
 		{
@@ -101,13 +104,27 @@ namespace Novell.iFolder
 			this.Icon = new Gdk.Pixbuf(Util.ImagesPath("ifolder.png"));
 			this.HasSeparator = false;
 //			this.BorderWidth = 10;
-			this.Resizable = false;
+			this.SetDefaultSize (450, 100);
+	//		this.Resizable = false;
 			this.Modal = true;
 			this.DefaultResponse = ResponseType.Ok;
 
-			Image iFolderImage = new Image(
+			//-----------------------------
+			// Add iFolderGraphic
+			//-----------------------------
+			HBox imagebox = new HBox();
+			imagebox.Spacing = 0;
+			iFolderBanner = new Image(
 					new Gdk.Pixbuf(Util.ImagesPath("ifolder-banner.png")));
-			this.VBox.PackStart (iFolderImage, false, false, 0);
+			imagebox.PackStart(iFolderBanner, false, false, 0);
+
+			ScaledPixbuf = 
+				new Gdk.Pixbuf(Util.ImagesPath("ifolder-banner-scaler.png"));
+			iFolderScaledBanner = new Image(ScaledPixbuf);
+			iFolderScaledBanner.ExposeEvent += 
+					new ExposeEventHandler(OnBannerExposed);
+			imagebox.PackStart(iFolderScaledBanner, true, true, 0);
+			this.VBox.PackStart (imagebox, false, true, 0);
 	
 			Table loginTable;
 
@@ -203,6 +220,28 @@ namespace Novell.iFolder
 			this.DefaultResponse = ResponseType.Ok;
 		}
 
+		private void OnBannerExposed(object o, ExposeEventArgs args)
+		{
+			if(args.Event.Count > 0)
+				return;
+
+			Gdk.Pixbuf spb = 
+				ScaledPixbuf.ScaleSimple(iFolderScaledBanner.Allocation.Width,
+										iFolderScaledBanner.Allocation.Height,
+										Gdk.InterpType.Nearest);
+
+			Gdk.GC gc = new Gdk.GC(iFolderScaledBanner.GdkWindow);
+
+			spb.RenderToDrawable(iFolderScaledBanner.GdkWindow,
+											gc,
+											0, 0,
+											args.Event.Area.X,
+											args.Event.Area.Y,
+											args.Event.Area.Width,
+											args.Event.Area.Height,
+											Gdk.RgbDither.Normal,
+											0, 0);
+		}
 
 		private void OnFieldsChanged(object obj, EventArgs args)
 		{
