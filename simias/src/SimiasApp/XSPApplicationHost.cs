32a108037194cb029b2e85abc4a8e959325f30de
ryoung65 <ryoung65@db2431b9-df06-0410-971c-a38ea9a75485>
2005-02-02 20:36:08 +0000

    Checked in the XSP fixes. to return 401 bad request when the request is not understood.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@3401 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/SimiasApp/XSPApplicationHost.cs
+++ simias/src/SimiasApp/XSPApplicationHost.cs
@@ -228,7 +228,17 @@ namespace Mono.ASPNET
 					return;
 
 				InitialWorkerRequest ir = new InitialWorkerRequest (stream);
-				ir.ReadRequestData ();
+				try {
+					ir.ReadRequestData ();
+				} catch (Exception ex) {
+					if (ir.GotSomeInput) {
+						byte [] badReq = HttpErrors.BadRequest ();
+						Write (badReq, 0, badReq.Length);
+					}
+
+					throw ex;
+				}
+
 				RequestData rdata = ir.RequestData;
 				string vhost = null; // TODO: read the headers in InitialWorkerRequest
 				int port = ((IPEndPoint) localEP).Port;
