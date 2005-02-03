5e6ac5548fb8c35aff1bde2700b13145b352e566
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2005-02-03 16:13:42 +0000

    updated to fixe "calvin" dependency
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@3419 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/src/OSXClient/src/PropSharingController.m
+++ ifolder/src/OSXClient/src/PropSharingController.m
@@ -1,6 +1,53 @@
 #import "PropSharingController.h"
+#import "iFolderService.h"
+#import "MainWindowController.h"
 
 @implementation PropSharingController
+
+
+-(void)awakeFromNib
+{
+	ifolderService = [[iFolderService alloc] init];
+
+	keyedUsers = [[NSMutableDictionary alloc] init];
+
+	NSLog(@"Reading all iFolder Users");
+
+	@try
+	{
+		int userCount;
+		ifolderID = [[NSApp delegate] seletediFolderID];
+		if(ifolderID != nil)
+		{
+			NSArray *newUsers = [ifolderService GetiFolderUsers:curiFolder ];
+			if(newUsers != nil)
+			{
+				for(userCount = 0; userCount < [newUsers count]; userCount++)
+				{
+					User *newUser = [newUsers objectAtIndex:userCount];
+
+					[self addUser:newUser];
+				}
+			}
+		}
+	}
+	@catch (NSException *e)
+	{
+		[[NSApp delegate] addLog:@"Reading iFolder Users failed with an exception"];
+	}
+}
+
+
+-(void) addUser:(User *)newUser
+{
+	if([keyedUsers objectForKey:[newUser UserID]] == nil)
+	{
+		[usersController addObject:newUser];
+		[keyedUsers setObject:newUser forKey:[newUser UserID] ];
+	}
+}
+
+
 
 - (IBAction)addSelectedUsers:(id)sender
 {
@@ -19,7 +66,29 @@
 
 - (IBAction)searchUsers:(id)sender
 {
-	NSLog(@"Searching for users");
+	@try
+	{
+		if(ifolderID != nil)
+		{
+			NSArray *newUsers = [ifolderService GetiFolderUsers:curiFolder ];
+			if(newUsers != nil)
+			{
+				for(userCount = 0; userCount < [newUsers count]; userCount++)
+				{
+					User *newUser = [newUsers objectAtIndex:userCount];
+
+					[self addUser:newUser];
+				}
+			}
+		}
+	}
+	@catch (NSException *e)
+	{
+		[[NSApp delegate] addLog:@"Reading iFolder Users failed with an exception"];
+	}
+
+	foundUsersController	
+
 }
 
 @end
