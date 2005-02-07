6086c37238dcfe7971b6a8ed8d4cc4562f50617e
ryoung65 <ryoung65@db2431b9-df06-0410-971c-a38ea9a75485>
2005-02-07 22:06:19 +0000

    Fixed a bug dealing with illegal file name characters.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@3493 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/Sync/Conflict.cs
+++ simias/src/Sync/Conflict.cs
@@ -288,6 +288,9 @@ public class Conflict
 			//TODO: what if move succeeds but node rename or commit fails?
 			File.Move(FileNameConflictPath, Path.Combine(Path.GetDirectoryName(NonconflictedPath), newNodeName));
 			node.Name = newNodeName;
+			string relativePath = node.Properties.GetSingleProperty(PropertyTags.FileSystemPath).Value.ToString();
+			relativePath = relativePath.Remove(relativePath.Length - node.Name.Length -1, node.Name.Length) + newNodeName;
+			node.Properties.ModifyNodeProperty(new Property(PropertyTags.FileSystemPath, Syntax.String, relativePath));
 		}
 		Node newNode = collection.DeleteCollision(node);
 		newNode.Properties.DeleteSingleProperty(ConflictNameProperty);
