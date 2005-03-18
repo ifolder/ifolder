15b0b1d97ae1cc22b4215cd6be8bf4df277c2ebc
(no author) <(no author)@db2431b9-df06-0410-971c-a38ea9a75485>
2005-03-18 22:44:34 +0000

    This commit was manufactured by cvs2svn to create branch 'GAIM_DOMAIN_20050321'.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/GAIM_DOMAIN_20050321@4117 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/SimiasApp/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyVersion("1.0.4")]
-[assembly: AssemblyTitle ("Mono-XSP Server")]
-[assembly: AssemblyDescription ("Minimalistic web server for testing System.Web")]
-[assembly: AssemblyCopyright ("2002,2003 Ximian, Inc.\n(c) 2003,2004 Novell, Inc.")]
-[assembly: AssemblyCompany ("Novell, Inc.")]
