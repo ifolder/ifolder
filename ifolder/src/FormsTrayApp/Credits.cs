caf80d53c1393f34f70838c51535402fa66408e2
bgetter <bgetter@db2431b9-df06-0410-971c-a38ea9a75485>
2005-03-25 17:51:36 +0000

    Code cleanup.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@4185 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/src/FormsTrayApp/Credits.cs
+++ ifolder/src/FormsTrayApp/Credits.cs
@@ -51,10 +51,6 @@ namespace Novell.FormsTrayApp
 			// Required for Windows Form Designer support
 			//
 			InitializeComponent();
-
-			//
-			// TODO: Add any constructor code after InitializeComponent call
-			//
 		}
 
 		/// <summary>
