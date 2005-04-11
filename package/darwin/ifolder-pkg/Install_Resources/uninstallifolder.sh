723384a5d0ab8867d606deeceb92a5ab8508ef51
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2005-04-11 22:11:54 +0000

    Bug 75963: Added an uninstall script
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk/ifolder@4385 db2431b9-df06-0410-971c-a38ea9a75485

---
+++ package/darwin/ifolder-pkg/Install_Resources/uninstallifolder.sh
@@ -0,0 +1,9 @@
+#!/bin/sh -x
+
+#This script removes iFolder from an OS X System.  It must be run as root
+
+rm -rf /opt/novell/ifolder3
+rm -rf /Applications/iFolder.app
+rm -rf ~/.local/share/simias
+rm -rf /Library/Receipts/iFolderClient.pkg
+
