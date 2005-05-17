97b3cf1a28d6bf93e82ea57371be4a1b75c4a5e5
bgetter <bgetter@db2431b9-df06-0410-971c-a38ea9a75485>
2005-05-17 05:03:32 +0000

    Updated help files (fix for Bug 75948).
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@4618 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/help/en/doc/user/data/toc_front.js
+++ ifolder/help/en/doc/user/data/toc_front.js
@@ -10,14 +10,17 @@ function updateMenu_front() {
      level1ID = parent.theMenu.findEntry('data/front.html', 'url', 'right');
      if (level1ID == -1)
       {
-      level1ID = parent.theMenu.addChild(level0ID, "", "iFolder Help for Novell iFolder 3.0 Public Beta", "../doc/user/data/front.html", "zFLDR xB.0000.0000.", "iFolder Help for Novell iFolder 3.0 Public Beta");
-      level2ID = parent.theMenu.addChild(level1ID, "Folder", "Overview of iFolder", "../doc/user/data/bq1qyz2.html", "zFLDR xC.0000.0000.0001.", "Overview of iFolder");
+      level1ID = parent.theMenu.addChild(level0ID, "", "iFolder Help", "../doc/user/data/front.html", "zFLDR xB.0000.0000.", "iFolder Help");
+      level2ID = parent.theMenu.addChild(level1ID, "Folder", "Overview of iFolder", "../doc/user/data/overview.html", "zFLDR xC.0000.0000.0001.", "Overview of iFolder");
       parent.theMenu.entry[level2ID].FirstChild = -2;
-      parent.theMenu.entry[level2ID].onToggle = 'parent.theMenu.loadScript("../doc/user/data/bq1qyz2.html","../doc/user/data/toc_bq1qyz2.html", true)';
-      level2ID = parent.theMenu.addChild(level1ID, "Folder", "Managing iFolders", "../doc/user/data/myifolders.html", "zFLDR xC.0000.0000.0002.", "Managing iFolders");
+      parent.theMenu.entry[level2ID].onToggle = 'parent.theMenu.loadScript("../doc/user/data/overview.html","../doc/user/data/toc_overview.html", true)';
+      level2ID = parent.theMenu.addChild(level1ID, "Folder", "Managing iFolder Accounts and Preferences", "../doc/user/data/accountsprefs.html", "zFLDR xC.0000.0000.0002.", "Managing iFolder Accounts and Preferences");
+      parent.theMenu.entry[level2ID].FirstChild = -2;
+      parent.theMenu.entry[level2ID].onToggle = 'parent.theMenu.loadScript("../doc/user/data/accountsprefs.html","../doc/user/data/toc_accountsprefs.html", true)';
+      level2ID = parent.theMenu.addChild(level1ID, "Folder", "Managing iFolders", "../doc/user/data/myifolders.html", "zFLDR xC.0000.0000.0003.", "Managing iFolders");
       parent.theMenu.entry[level2ID].FirstChild = -2;
       parent.theMenu.entry[level2ID].onToggle = 'parent.theMenu.loadScript("../doc/user/data/myifolders.html","../doc/user/data/toc_myifolders.html", true)';
-      level2ID = parent.theMenu.addChild(level1ID, "Document", "Legal Notices", "../doc/user/data/legal.html", "zHTML xC.0000.0000.0003.", "Legal Notices");
+      level2ID = parent.theMenu.addChild(level1ID, "Document", "Legal Notices", "../doc/user/data/legal.html", "zHTML xC.0000.0000.0004.", "Legal Notices");
       parent.theMenu.entry[level1ID].setSelected(true);
       parent.theMenu.reload();
 
