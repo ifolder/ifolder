488d62154c3651df5f71c49ccd279c07569fb8e6
ryoung65 <ryoung65@db2431b9-df06-0410-971c-a38ea9a75485>
2005-05-18 21:51:01 +0000

    Added code to deal with the 2 second modified time resolution on FAT32. https://bugzilla.novell.com/show_bug.cgi?id=84526
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@4644 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/Sync/SyncClient.cs
+++ simias/src/Sync/SyncClient.cs
@@ -689,7 +689,9 @@ namespace Simias.Sync
 				}
 
 				// We may have just created or deleted nodes wait for the events to settle.
-				Thread.Sleep(500);
+				// We will wait for 2 seconds because of file time resolution on fat32
+				// This will ensure that we don't miss any changes.
+				Thread.Sleep(2000);
 				
 				// Setup the url to the server.
 				string userID = store.GetUserIDFromDomainID(collection.Domain);
