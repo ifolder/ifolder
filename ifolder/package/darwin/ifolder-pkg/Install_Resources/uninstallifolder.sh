aa91f0db9d9e14c583aa9e5b6daec0f8a38cb6df
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2005-06-01 14:43:46 +0000

    updated packages to install in ifolder3 and changed all strings too
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@4740 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/package/darwin/ifolder-pkg/Install_Resources/uninstallifolder.sh
+++ ifolder/package/darwin/ifolder-pkg/Install_Resources/uninstallifolder.sh
@@ -2,8 +2,8 @@
 
 #This script removes iFolder from an OS X System.  It must be run as root
 
-rm -rf /opt/novell/ifolder-se
-rm -rf /Applications/iFolder.app
+rm -rf /opt/novell/ifolder3
+rm -rf "/Applications/iFolder 3.app"
 rm -rf ~/.local/share/simias
 rm -rf /Library/Receipts/iFolderClient.pkg
 rm -rf /Library/Receipts/Simias.pkg
