d66bff0b57add1e2d84d0e204209d22d1709bfb1
mlasky <mlasky@db2431b9-df06-0410-971c-a38ea9a75485>
2005-06-03 16:03:09 +0000

    1. Code changes for bug #78523. Merged from R1FCSRC1_BR.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@4770 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/src/Common/WebState.cs
+++ simias/src/Common/WebState.cs
@@ -35,6 +35,179 @@ using Simias.Client.Event;
 namespace Simias
 {
 	/// <summary>
+	/// Class to share and keep proxy state for web communications.
+	/// </summary>
+	public class ProxyState : IWebProxy
+	{
+		#region Class Members
+
+		private IWebProxy webProxy;
+		private static Hashtable proxyHash = new Hashtable();
+
+		#endregion
+
+		#region Constructor
+
+		/// <summary>
+		/// Initializes an instance of the object.
+		/// </summary>
+		/// <param name="proxy">The proxy server address.</param>
+		/// <param name="proxyUser">The user name for proxy authentication.</param>
+		/// <param name="proxyPassword">The password for proxy authentication.</param>
+		private ProxyState( Uri proxy, string proxyUser, string proxyPassword )
+		{
+			InitializeWebProxy( proxy, proxyUser, proxyPassword );
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Initializes the WebProxy object. 
+		/// </summary>
+		/// <param name="proxy">The proxy server address.</param>
+		/// <param name="proxyUser">The user name for proxy authentication.</param>
+		/// <param name="proxyPassword">The password for proxy authentication.</param>
+		private void InitializeWebProxy( Uri proxy, string proxyUser, string proxyPassword )
+		{
+			if ( proxy != null )
+			{
+				if ( ( proxyUser == null ) || ( proxyUser == String.Empty ) )
+				{
+					webProxy = new WebProxy( proxy, false );
+				}
+				else
+				{
+					webProxy = new WebProxy( 
+						proxy, 
+						false, 
+						new string[] {}, 
+						new NetworkCredential( proxyUser, proxyPassword, proxy.ToString() ) );
+				}
+			}
+			else
+			{
+				webProxy = GlobalProxySelection.GetEmptyWebProxy();
+			}
+		}
+
+		/// <summary>
+		/// Creates a proxy key to use for the specified host.
+		/// </summary>
+		/// <param name="server">Uri of the host.</param>
+		/// <returns></returns>
+		private static string ProxyKey( Uri server )
+		{
+			return new UriBuilder( server.Scheme, server.Host.ToLower(), server.Port ).Uri.ToString();
+		}
+
+		#endregion
+
+		#region Public Methods
+
+		/// <summary>
+		/// Adds a ProxyState object for the specified server address.
+		/// </summary>
+		/// <param name="server">The simias server address.</param>
+		public static ProxyState AddProxyState( Uri server )
+		{
+			return AddProxyState( server, null, null, null );
+		}
+
+		/// <summary>
+		/// Adds a ProxyState object for the specified server address.
+		/// </summary>
+		/// <param name="server">The simias server address.</param>
+		/// <param name="proxy">The proxy server address.</param>
+		/// <param name="proxyUser">The user name for proxy authentication.</param>
+		/// <param name="proxyPassword">The password for proxy authentication.</param>
+		public static ProxyState AddProxyState( Uri server, Uri proxy, string proxyUser, string proxyPassword )
+		{
+			lock( proxyHash )
+			{
+				string key = ProxyKey( server );
+				ProxyState ps = proxyHash[ key ] as ProxyState;
+				if ( ps != null )
+				{
+					ps.InitializeWebProxy( proxy, proxyUser, proxyPassword );
+				}
+				else
+				{
+					ps = new ProxyState( proxy, proxyUser, proxyPassword );
+					proxyHash[ key ] = ps;
+				}
+
+				return ps;
+			}
+		}
+
+		/// <summary>
+		/// Deletes the specified ProxyState object for the specified server address.
+		/// </summary>
+		/// <param name="server">Address of the server to delete a ProxyState object for.</param>
+		public static void DeleteProxyState( Uri server )
+		{
+			lock ( proxyHash )
+			{
+				proxyHash[ ProxyKey( server ) ] = null;
+			}
+		}
+
+		/// <summary>
+		/// Gets a ProxyState object for the specified server address.
+		/// </summary>
+		/// <param name="server">Address of the server to find a ProxyState object for.</param>
+		/// <returns>A corresponding ProxyState object.</returns>
+		public static ProxyState GetProxyState( Uri server )
+		{
+			ProxyState ps;
+
+			lock ( proxyHash )
+			{
+				ps = proxyHash[ ProxyKey( server ) ] as ProxyState;
+			}
+
+			return ( ps == null ) ? AddProxyState( server ) : ps;
+		}
+
+		#endregion
+
+		#region IWebProxy Members
+
+		/// <summary>
+		/// Returns the URI of a proxy.
+		/// </summary>
+		/// <param name="destination">A Uri specifying the requested Internet resource.</param>
+		/// <returns>A Uri containing the URI of the proxy used to contact destination.</returns>
+		public Uri GetProxy( Uri destination )
+		{
+			return webProxy.GetProxy( destination );
+		}
+
+		/// <summary>
+		/// The credentials to submit to the proxy server for authentication.
+		/// </summary>
+		public ICredentials Credentials
+		{
+			get { return webProxy.Credentials; }
+			set	{ webProxy.Credentials = value; }
+		}
+
+		/// <summary>
+		/// Indicates that the proxy should not be used for the specified host.
+		/// </summary>
+		/// <param name="host">The Uri of the host to check for proxy use.</param>
+		/// <returns>true if the proxy server should not be used for host; otherwise, false.</returns>
+		public bool IsBypassed( Uri host )
+		{
+			return webProxy.IsBypassed( host );
+		}
+
+		#endregion
+	}
+
+	/// <summary>
 	/// Class to share and keep the client state for web communications.
 	/// </summary>
 	public class WebState
@@ -43,28 +216,8 @@ namespace Simias
 			+ System.Reflection.Assembly.GetCallingAssembly().ImageRuntimeVersion 
 			+ " OS=" 
 			+ System.Environment.OSVersion.ToString();
-		IWebProxy					proxy;
 		NetworkCredential			credentials;
 		static Hashtable			cookieHash = new Hashtable();
-
-		/*
-		/// <summary>
-		/// Get a WebState object for the specified domain.
-		/// </summary>
-		/// <param name="domainID">The domain ID.</param>
-		/// <param name="memberID">The member the client is running as.</param>
-		public WebState(string domainID, string memberID) :
-			this()
-		{
-			// Get the credentials for this collection.
-			credentials = new Credentials(domainID, memberID).GetCredentials();
-			if (credentials == null)
-			{
-				new EventPublisher().RaiseEvent(new NeedCredentialsEventArgs(domainID, memberID));
-				throw new NeedCredentialsException();
-			}
-		}
-		*/
 
 		/// <summary>
 		/// Get a WebState object for the specified domain and collection.
@@ -119,8 +272,6 @@ namespace Simias
 					cookieHash[ domainID ] = new CookieContainer();
 				}
 			}
-
-			proxy = null;
 		}
 		
 		/// <summary>
@@ -133,7 +284,7 @@ namespace Simias
 			request.UserAgent = userAgent;
 			request.Credentials = credentials;
 			request.CookieContainer = cookieHash[ domainID ] as CookieContainer;
-			// request.Proxy = proxy;
+			request.Proxy = ProxyState.GetProxyState( request.RequestUri );
 			request.PreAuthenticate = true;
 		}
 
@@ -147,7 +298,7 @@ namespace Simias
 			request.UserAgent = userAgent;
 			request.Credentials = credentials;
 			request.CookieContainer = cookieHash[ domainID ] as CookieContainer;
-			// request.Proxy = proxy;
+			request.Proxy = ProxyState.GetProxyState( new Uri( request.Url ) );
 			request.PreAuthenticate = true;
 		}
 
--- simias/src/SimiasClient/CertPolicy.cs
+++ simias/src/SimiasClient/CertPolicy.cs
@@ -172,6 +172,7 @@ namespace Simias.Client
 		public bool CheckValidationResult( ServicePoint srvPoint, X509Certificate certificate, WebRequest request, int certificateProblem )
 		{
 			bool honorCert = false;
+			string localHost = srvPoint.Address.Host;
 
 			if ((certificateProblem == 0) || (CertificateProblem.CertEXPIRED.Equals(certificateProblem)))
 			{
@@ -179,15 +180,33 @@ namespace Simias.Client
 			}
 			else
 			{
-				CertificateState cs = CertTable[srvPoint.Address.Host] as CertificateState;
+			CheckForRealHost:
+				CertificateState cs = CertTable[localHost] as CertificateState;
 				if (cs != null && cs.IsValid(certificate))
 				{
 					honorCert = true;
 				}
 				else
 				{
-					// This is a new cert replace the certificate.
-					CertTable[srvPoint.Address.Host] = new CertificateState(certificate, false, (CertificateProblem)certificateProblem);
+					if ( !request.Proxy.IsBypassed( request.RequestUri ) )
+					{
+						string requestHost = request.Proxy.GetProxy( request.RequestUri ).Host;
+						if (String.Compare(requestHost, localHost, true) == 0)
+						{
+							localHost = request.RequestUri.Host;
+							goto CheckForRealHost;
+						}
+						else
+						{
+							// This is a new cert replace the certificate.
+							CertTable[localHost] = new CertificateState(certificate, false, (CertificateProblem)certificateProblem);
+						}
+					}
+					else
+					{
+						// This is a new cert replace the certificate.
+						CertTable[localHost] = new CertificateState(certificate, false, (CertificateProblem)certificateProblem);
+					}
 				}
 			}
 			return honorCert;
