7e16ee207bf4bcde0d64c48fb2c1d2eb3b71ed8b
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2005-06-23 03:54:47 +0000

    BUG 78572 Updated strings in Accept Cert to be the same
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@4858 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/src/OSXClient/src/AcceptCertSheetController.m
+++ ifolder/src/OSXClient/src/AcceptCertSheetController.m
@@ -3,14 +3,16 @@
 @implementation AcceptCertSheetController
 
 
-- (AcceptCertSheetController *) initWithCert:(SecCertificateRef)CertRef
+- (AcceptCertSheetController *) initWithCert:(SecCertificateRef)CertRef forHost:(NSString *)Host
 {
 	if ((self = [super initWithWindowNibName:@"AcceptCert"]) != nil)
 	{
 		certRef = CertRef;
+		host = Host;
 	}
 	return self;
 }
+
 
 //===================================================================
 // awakeFromNib
@@ -22,6 +24,14 @@
 	[certView setCertificate:certRef];
 	[certView setDisplayTrust:NO];
 	[certView setEditableTrust:NO];
+
+	[messageTitle setStringValue:[NSString stringWithFormat:
+		NSLocalizedString(@"iFolder cannot verify the identity of the iFolder Server \"%@\".", 
+			@"Accept Certificate Message"), host]];
+
+	[message setStringValue:[NSString stringWithFormat:
+		NSLocalizedString(@"The certificate for this iFolder Server was signed by an unknown certifying authority.  You might be connecting to a server that is pretending to be \"%@\" which could put your confidential information at risk.   Before accepting this certificate, you should check with your system administrator.  Do you want to accept this certificate permanently and continue to connect?", 
+			@"Accept Certificate Details"), host]];
 }
 
 - (IBAction)accept:(id)sender
