7568fcfd6302a411f4f268dfb4b0ad0ea0d5e767
bgetter <bgetter@db2431b9-df06-0410-971c-a38ea9a75485>
2005-07-11 21:39:30 +0000

    Updated help files.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk/ifolder@4960 db2431b9-df06-0410-971c-a38ea9a75485

--- help/en/doc/user/data/toc_overview.js
+++ help/en/doc/user/data/toc_overview.js
@@ -10,7 +10,7 @@ function updateMenu_overview() {
      level2ID = parent.theMenu.findEntry('data/overview.html', 'url', 'right');
      if ((level2ID != -1) && (parent.theMenu.entry[level2ID].FirstChild < 0))
       {
-      level3ID = parent.theMenu.addChild(level2ID, "Document", "Enterprise Server Sharing", "../doc/user/data/bv1g15h.html", "zHTML xD.0000.0000.0001.0001.", "Enterprise Server Sharing");
+      level3ID = parent.theMenu.addChild(level2ID, "Document", "iFolder Sharing", "../doc/user/data/bx3aueg.html", "zHTML xD.0000.0000.0001.0001.", "iFolder Sharing");
       level3ID = parent.theMenu.addChild(level2ID, "Document", "Benefits of iFolder", "../doc/user/data/bq1r10z.html", "zHTML xD.0000.0000.0001.0002.", "Benefits of iFolder");
       level3ID = parent.theMenu.addChild(level2ID, "Document", "Key Features of iFolder", "../doc/user/data/bq1r110.html", "zHTML xD.0000.0000.0001.0003.", "Key Features of iFolder");
       level3ID = parent.theMenu.addChild(level2ID, "Document", "Cross-Platform Considerations", "../doc/user/data/bwcku86.html", "zHTML xD.0000.0000.0001.0004.", "Cross-Platform Considerations");
