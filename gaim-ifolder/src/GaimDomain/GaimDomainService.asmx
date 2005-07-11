62a2f2a09b17867ef12b702e0e69ef19b586e61a
(no author) <(no author)@db2431b9-df06-0410-971c-a38ea9a75485>
2005-07-11 20:31:56 +0000

    This commit was manufactured by cvs2svn to create tag 'BHT-pre-event-based-linux'.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/tags/BHT-pre-event-based-linux@4956 db2431b9-df06-0410-971c-a38ea9a75485

--- gaim-ifolder/src/GaimDomain/GaimDomainService.asmx
+++
@@ -1,1 +0,0 @@
-<%@ WebService Language="c#" Codebehind="GaimDomainService.asmx.cs" Class="Simias.Gaim.DomainService.GaimDomainService,Simias.Gaim" %>
--- nautilus-ifolder/external/gsoap-linux-2.7/samples/ck/ck.h
+++
@@ -1,8 +0,0 @@
-//gsoap ck service name:	ck
-//gsoap ck service style:	rpc
-//gsoap ck service encoding:	encoded
-//gsoap ck service location:	http://www.cs.fsu.edu/~engelen/ck.cgi
-//gsoap ck service namespace:	http://www.cs.fsu.edu/~engelen/ck.wsdl
-
-//gsoap ck schema  namespace:	urn:ck
-int ck__demo(char **r);
