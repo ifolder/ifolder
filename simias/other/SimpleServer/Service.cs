d5b5fd6d63674b51464a6172d574b6d09f5b6b8b
bradya <bradya@db2431b9-df06-0410-971c-a38ea9a75485>
2005-07-26 16:17:22 +0000

    fixed IThreadService Custom method to return an int
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@4995 db2431b9-df06-0410-971c-a38ea9a75485

--- simias/other/SimpleServer/Service.cs
+++ simias/other/SimpleServer/Service.cs
@@ -105,8 +105,9 @@ namespace Simias.SimpleServer
 		/// </summary>
 		/// <param name="message"></param>
 		/// <param name="data"></param>
-		public void Custom(int message, string data)
+		public int Custom(int message, string data)
 		{
+			return 0;
 		}
 
 		/// <summary>
