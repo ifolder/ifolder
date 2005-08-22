f94ddfadc260a45d7ff29a6e3e4322ca39aba1e4
btimothy <btimothy@db2431b9-df06-0410-971c-a38ea9a75485>
2005-08-22 16:05:29 +0000

    IThreadService.cs changed on me (the "Custom()" function now returns an int instead of being void).  I had to change this for it to function correctly.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@5110 db2431b9-df06-0410-971c-a38ea9a75485

--- gaim-ifolder/src/GaimDomain/GaimService.cs
+++ gaim-ifolder/src/GaimDomain/GaimService.cs
@@ -161,8 +161,9 @@ namespace Simias.Gaim
 		/// </summary>
 		/// <param name="message"></param>
 		/// <param name="data"></param>
-		public void Custom(int message, string data)
+		public int Custom(int message, string data)
 		{
+			return 0;
 		}
 
 		/// <summary>
