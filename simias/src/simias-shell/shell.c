ec16e9339212324a2d02d30e95aa40f12c95c787
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2005-10-11 19:09:51 +0000

    adding simias-shell
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/APIWORK@5383 db2431b9-df06-0410-971c-a38ea9a75485

---
+++ simias/src/simias-shell/shell.c
@@ -0,0 +1,219 @@
+/* Sample Shell app for Simias APIs */
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "simias.h"
+
+// Globals for shell environment
+SimiasHandle hSimias;
+SimiasNode	 hDomain;
+SimiasNode	 hCurNode;
+int level = 0;
+
+
+int doList(void);
+int doCD(char *name);
+
+
+int doCD(char *name)
+{
+	int rc = 0;
+	SimiasNodeList hNodeList;
+	SimiasNode		hNewNode;
+
+	printf("Changing into %s\n", name);
+	if(strncmp(name, "..", 2) == 0)
+	{
+		if(level == 0)
+			return 0;
+		else if(level == 1)
+		{
+			simias_node_free(hCurNode);
+			hDomain = NULL;
+			level = 0;
+			return 0;
+		}
+		else if(level == 2)
+		{
+			simias_node_free(hCurNode);
+			hCurNode = hDomain;
+			level = 1;
+			return 0;
+		}
+	}
+
+
+	// if level is 0, we are at the top
+	if(level == 0)
+	{
+		printf("Getting domain list...\n");
+		rc = simias_get_domains(hSimias, &hNodeList);
+		if(rc)
+			return rc;
+	}
+	else if(level == 1)
+	{
+		rc = simias_get_collections_for_domain(hSimias, 
+											   &hNodeList, 
+											   simias_node_get_id(hCurNode));
+		if(rc)
+			return rc;
+	}
+	else if(level == 2)
+	{
+		printf("Error: the object %s has no containment\n", 
+					simias_node_get_name(hCurNode));
+	}
+
+
+	rc = simias_nodelist_extract_node_by_name(hNodeList,
+											  &hNewNode,
+											  name);
+	if(rc)
+	{
+		printf("Error extracting node by name\n");
+		simias_nodelist_free(&hNodeList);
+		return rc;
+	}
+
+	if(hNewNode != NULL)
+	{
+		if(level == 0)
+			hDomain = hCurNode;
+		else
+			simias_node_free(hCurNode);
+		level++;
+		hCurNode = hNewNode;
+	}
+
+	rc = simias_nodelist_free(&hNodeList);
+	if(rc)
+		return rc;
+}
+
+
+
+int doList(void)
+{
+	int rc = 0;
+	SimiasNodeList hNodeList;
+	int nodeCounter, nodeCount = 0;
+
+	if(level == 0)
+	{
+		rc = simias_get_domains(hSimias, &hNodeList);
+		if(rc)
+			return rc;
+	}
+	else if(level == 1)
+	{
+		rc = simias_get_collections_for_domain(hSimias, 
+											   &hNodeList, 
+											   simias_node_get_id(hCurNode));
+		if(rc)
+			return rc;
+	}
+	else
+	{
+		rc = simias_get_nodes(hSimias, 
+								&hNodeList, 
+								simias_node_get_id(hCurNode));
+		if(rc)
+			return rc;
+	}
+
+	rc = simias_nodelist_get_node_count(hNodeList, &nodeCount);
+	if(rc)
+	{
+		simias_nodelist_free(&hNodeList);
+		return rc;
+	}
+
+	for(nodeCounter = 0; nodeCounter < nodeCount; nodeCounter++)
+	{
+		SimiasNode hNode;
+
+		rc = simias_nodelist_extract_node(hNodeList, &hNode,
+												nodeCounter);
+		if(rc)
+		{
+			simias_nodelist_free(&hNodeList);
+			return rc;
+		}
+
+		printf("%s\n", simias_node_get_name(hNode));
+
+		rc = simias_node_free(&hNode);
+		if(rc)
+		{
+			simias_nodelist_free(&hNodeList);
+			return rc;
+		}
+	}
+
+	rc = simias_nodelist_free(&hNodeList);
+	if(rc)
+		return rc;
+}
+
+
+int shell_prompt()
+{
+	char buffer[256];
+	bool reprompt = true;
+
+	while(reprompt)
+	{
+		if(hCurNode == NULL)
+			printf("simias [root]: ");
+		else
+			printf("simias [%s]: ", simias_node_get_name(hCurNode));
+
+		memset(buffer, 0, 256);
+		fgets(buffer, 256, stdin);
+
+		if(strncmp(buffer, "ls", 2) == 0)
+			doList();
+		if(strncmp(buffer, "cd", 2) == 0)
+		{
+			char *name = strchr(buffer, ' ');
+			if(name != NULL)
+				doCD(name++);
+		}
+		else if(strncmp(buffer, "quit", 4) == 0)
+			reprompt = false;
+		else if(strncmp(buffer, "q", 1) == 0)
+			reprompt = false;
+	}
+}
+
+
+int main(int argc, char **argv)
+{
+	printf("Connecting to simias...");
+	hSimias = NULL;
+	hDomain = NULL;
+	hCurNode = NULL;
+	level = 0;
+
+	int rc = simias_init_local(&hSimias);
+	if(rc)
+		return rc;
+
+	printf("done\n");
+	shell_prompt();
+
+	rc = simias_free(&hSimias);
+	if(rc)
+	{
+		printf("Error simias_free: %d\n", rc);
+	}
+	
+	return 0;
+}
+
+
+
