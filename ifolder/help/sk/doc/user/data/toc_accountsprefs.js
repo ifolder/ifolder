5ae9fbb2e02c289c0c6b7a47751fe4170e7b48b5
tmellor <tmellor@db2431b9-df06-0410-971c-a38ea9a75485>
2005-12-02 18:08:33 +0000

    Updated localization files.  Previously english placeholders
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@5703 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/help/sk/doc/user/data/toc_accountsprefs.js
+++ ifolder/help/sk/doc/user/data/toc_accountsprefs.js
@@ -10,14 +10,14 @@ function updateMenu_accountsprefs() {
      level2ID = parent.theMenu.findEntry('data/accountsprefs.html', 'url', 'right');
      if ((level2ID != -1) && (parent.theMenu.entry[level2ID].FirstChild < 0))
       {
-      level3ID = parent.theMenu.addChild(level2ID, "Document", "Starting the iFolder Client", "../doc/user/data/bw08fxi.html", "zHTML xD.0000.0000.0002.0001.", "Starting the iFolder Client");
-      level3ID = parent.theMenu.addChild(level2ID, "Document", "Configuring an iFolder Account", "../doc/user/data/accounts.html", "zHTML xD.0000.0000.0002.0002.", "Configuring an iFolder Account");
-      level3ID = parent.theMenu.addChild(level2ID, "Document", "Logging In to an iFolder Account", "../doc/user/data/login.html", "zHTML xD.0000.0000.0002.0003.", "Logging In to an iFolder Account");
-      level3ID = parent.theMenu.addChild(level2ID, "Document", "Viewing and Modifying iFolder Account Settings", "../doc/user/data/accountdetails.html", "zHTML xD.0000.0000.0002.0004.", "Viewing and Modifying iFolder Account Settings");
-      level3ID = parent.theMenu.addChild(level2ID, "Document", "Deleting an iFolder Account", "../doc/user/data/bvwsp7c.html", "zHTML xD.0000.0000.0002.0005.", "Deleting an iFolder Account");
-      level3ID = parent.theMenu.addChild(level2ID, "Document", "Configuring iFolder Preferences for the Client", "../doc/user/data/preferences.html", "zHTML xD.0000.0000.0002.0006.", "Configuring iFolder Preferences for the Client");
-      level3ID = parent.theMenu.addChild(level2ID, "Document", "Configuring Local Firewall Settings for iFolder Traffic", "../doc/user/data/bvwppw5.html", "zHTML xD.0000.0000.0002.0007.", "Configuring Local Firewall Settings for iFolder Traffic");
-      level3ID = parent.theMenu.addChild(level2ID, "Document", "Configuring Local Virus Scanner Settings for iFolder Traffic", "../doc/user/data/bwbirb9.html", "zHTML xD.0000.0000.0002.0008.", "Configuring Local Virus Scanner Settings for iFolder Traffic");
+      level3ID = parent.theMenu.addChild(level2ID, "Document", "Spustenie klienta aplikácie iFolder", "../doc/user/data/bw08fxi.html", "zHTML xD.0000.0000.0002.0001.", "Spustenie klienta aplikácie iFolder");
+      level3ID = parent.theMenu.addChild(level2ID, "Document", "Konfigurácia konta aplikácie iFolder", "../doc/user/data/accounts.html", "zHTML xD.0000.0000.0002.0002.", "Konfigurácia konta aplikácie iFolder");
+      level3ID = parent.theMenu.addChild(level2ID, "Document", "Prihlásenie sa na konto aplikácie iFolder", "../doc/user/data/login.html", "zHTML xD.0000.0000.0002.0003.", "Prihlásenie sa na konto aplikácie iFolder");
+      level3ID = parent.theMenu.addChild(level2ID, "Document", "Zobrazenie a zmena nastavenia konta aplikácie iFolder", "../doc/user/data/accountdetails.html", "zHTML xD.0000.0000.0002.0004.", "Zobrazenie a zmena nastavenia konta aplikácie iFolder");
+      level3ID = parent.theMenu.addChild(level2ID, "Document", "Odstránenie konta aplikácie iFolder", "../doc/user/data/bvwsp7c.html", "zHTML xD.0000.0000.0002.0005.", "Odstránenie konta aplikácie iFolder");
+      level3ID = parent.theMenu.addChild(level2ID, "Document", "Konfigurácia preferencií aplikácie iFolder pre klienta", "../doc/user/data/preferences.html", "zHTML xD.0000.0000.0002.0006.", "Konfigurácia preferencií aplikácie iFolder pre klienta");
+      level3ID = parent.theMenu.addChild(level2ID, "Document", "Konfigurácia lokálnych nastavení brány firewall pre prevádzku aplikácie iFolder", "../doc/user/data/bvwppw5.html", "zHTML xD.0000.0000.0002.0007.", "Konfigurácia lokálnych nastavení brány firewall pre prevádzku aplikácie iFolder");
+      level3ID = parent.theMenu.addChild(level2ID, "Document", "Konfigurácia lokálnych nastavení zisťovania vírusov pre prevádzku aplikácie iFolder", "../doc/user/data/bwbirb9.html", "zHTML xD.0000.0000.0002.0008.", "Konfigurácia lokálnych nastavení zisťovania vírusov pre prevádzku aplikácie iFolder");
       parent.theMenu.reload();
 
       }
--- ifolder/help/sk/doc/user/data/toc_front.js
+++ ifolder/help/sk/doc/user/data/toc_front.js
@@ -10,17 +10,17 @@ function updateMenu_front() {
      level1ID = parent.theMenu.findEntry('data/front.html', 'url', 'right');
      if (level1ID == -1)
       {
-      level1ID = parent.theMenu.addChild(level0ID, "", "iFolder Help", "../doc/user/data/front.html", "zFLDR xB.0000.0000.", "iFolder Help");
-      level2ID = parent.theMenu.addChild(level1ID, "Folder", "Overview of iFolder", "../doc/user/data/overview.html", "zFLDR xC.0000.0000.0001.", "Overview of iFolder");
+      level1ID = parent.theMenu.addChild(level0ID, "", "Pomocník aplikácie iFolder", "../doc/user/data/front.html", "zFLDR xB.0000.0000.", "Pomocník aplikácie iFolder");
+      level2ID = parent.theMenu.addChild(level1ID, "Folder", "Prehľad aplikácie iFolder", "../doc/user/data/overview.html", "zFLDR xC.0000.0000.0001.", "Prehľad aplikácie iFolder");
       parent.theMenu.entry[level2ID].FirstChild = -2;
       parent.theMenu.entry[level2ID].onToggle = 'parent.theMenu.loadScript("../doc/user/data/overview.html","../doc/user/data/toc_overview.html", true)';
-      level2ID = parent.theMenu.addChild(level1ID, "Folder", "Managing iFolder Accounts and Preferences", "../doc/user/data/accountsprefs.html", "zFLDR xC.0000.0000.0002.", "Managing iFolder Accounts and Preferences");
+      level2ID = parent.theMenu.addChild(level1ID, "Folder", "Správa kont a preferencií aplikácie iFolder", "../doc/user/data/accountsprefs.html", "zFLDR xC.0000.0000.0002.", "Správa kont a preferencií aplikácie iFolder");
       parent.theMenu.entry[level2ID].FirstChild = -2;
       parent.theMenu.entry[level2ID].onToggle = 'parent.theMenu.loadScript("../doc/user/data/accountsprefs.html","../doc/user/data/toc_accountsprefs.html", true)';
-      level2ID = parent.theMenu.addChild(level1ID, "Folder", "Managing iFolders", "../doc/user/data/myifolders.html", "zFLDR xC.0000.0000.0003.", "Managing iFolders");
+      level2ID = parent.theMenu.addChild(level1ID, "Folder", "Správa priečinkov iFolder", "../doc/user/data/myifolders.html", "zFLDR xC.0000.0000.0003.", "Správa priečinkov iFolder");
       parent.theMenu.entry[level2ID].FirstChild = -2;
       parent.theMenu.entry[level2ID].onToggle = 'parent.theMenu.loadScript("../doc/user/data/myifolders.html","../doc/user/data/toc_myifolders.html", true)';
-      level2ID = parent.theMenu.addChild(level1ID, "Document", "Legal Notices", "../doc/user/data/legal.html", "zHTML xC.0000.0000.0004.", "Legal Notices");
+      level2ID = parent.theMenu.addChild(level1ID, "Document", "Právne prehlásenia", "../doc/user/data/legal.html", "zHTML xC.0000.0000.0004.", "Právne prehlásenia");
       parent.theMenu.entry[level1ID].setSelected(true);
       parent.theMenu.reload();
 
