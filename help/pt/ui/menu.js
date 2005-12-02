7f1f6516d6f76c96ce25084e460875594fe8165e
tmellor <tmellor@db2431b9-df06-0410-971c-a38ea9a75485>
2005-12-02 18:08:17 +0000

    Updated localization files.  Previously english placeholders
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk/ifolder@5701 db2431b9-df06-0410-971c-a38ea9a75485

--- help/pt/ui/menu.js
+++ help/pt/ui/menu.js
@@ -1,4 +1,4 @@
-var theMenuRef = "parent.theMenu";
+﻿var theMenuRef = "parent.theMenu";
 var theMenu = eval(theMenuRef);
 var theBrowser = parent.theBrowser;
 var belowMenu = null;
@@ -236,12 +236,12 @@ function printSelectedTopics()
     }
     else 
     {   
-       alert("There are no items checked!");
+       alert("Não há itens marcados!");
     }
  }  
  else
  {
-    alert("There is no book to print.");
+    alert("Nenhum livro a ser impresso.");
  }  
 }
 
@@ -269,7 +269,7 @@ function searchSelectedTopics()
 
 		if (query == "")
 		{
-		   alert("No search parameter entered!");   
+		   alert("Nenhum parâmetro de pesquisa especificado!");   
 		}
 		else
 		{
@@ -306,13 +306,13 @@ function searchSelectedTopics()
 					}
 				}
 
-			    alert("There are no items checked!");
+			    alert("Não há itens marcados!");
 			}
 		}
 	 }  
 	 else
 	 {
-	    alert("There is no book to search.");
+	    alert("Nenhum livro a ser pesquisado.");
 	 }  
 }
  
