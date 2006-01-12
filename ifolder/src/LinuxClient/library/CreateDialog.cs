54ab1cd500ed19ce7e1e6fa624387e2080323d28
btimothy <btimothy@db2431b9-df06-0410-971c-a38ea9a75485>
2006-01-12 15:46:58 +0000

    Modified the accept/convert/download dialogs so they are more simple (i.e., user's aren't presented with a TextEntry box where they can type anything in).  They just have to select folders.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/ifolder_3_4@5815 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/src/LinuxClient/library/CreateDialog.cs
+++ ifolder/src/LinuxClient/library/CreateDialog.cs
@@ -78,7 +78,7 @@ namespace Novell.iFolder
 		/// filtering the list of domains, this parameter is used to allow this
 		/// dialog to respect the currently selected domain.
 		public CreateDialog(Gtk.Window parentWindow, DomainInformation[] domainArray, string filteredDomainID, string initialPath, iFolderWebService ifws)
-				: base("", Util.GS("Create a new iFolder..."), parentWindow, FileChooserAction.CreateFolder, Stock.Cancel, ResponseType.Cancel,
+				: base("", Util.GS("Select a folder..."), parentWindow, FileChooserAction.SelectFolder, Stock.Cancel, ResponseType.Cancel,
                 Stock.Ok, ResponseType.Ok)
 		{
 			domains = domainArray;
