91f7b8ea55a31d32f6da86efe08cc2ce63bb4439
btimothy <btimothy@db2431b9-df06-0410-971c-a38ea9a75485>
2006-03-29 21:29:24 +0000

    Fix for Bug 161970 - iFolder 3.4 - Reverting iFolder during first "Checking for Changes" causes simias events to be lost.
    The problem was that the client was not catching exceptions if they occurred while processing an event from Simias.  If this happens, the event server in simias disconnects the offending client so it doesn't have to deal with processing it anymore.
    
    I added try/catch statements in all of the places that process simias events to guarantee we never throw an exception.  I also had to change code to make sure we don't call ListStore.EmitRowChanged() when the TreeIter is no longer valid.
    
    Additionally, there's a new GConf setting "/apps/ifolder3/debug/print_simias_event_errors" that when set to TRUE will allow exceptions to be printed to the console.  The default setting for this is FALSE.  While adding this, I fixed a problem where the ClientConfig was looking for event notification using the "/apps/ifolder" instead of "/apps/ifolder3".
    
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/ifolder_3_4/ifolder@6098 db2431b9-df06-0410-971c-a38ea9a75485

--- src/LinuxClient/library/iFolderData.cs
+++ src/LinuxClient/library/iFolderData.cs
@@ -289,12 +289,9 @@ namespace Novell.iFolder
 								existingHolder.iFolder = ifolder;
 	
 								TreePath path = iFolderListStore.GetPath(iter);
-								iFolderListStore.EmitRowChanged(path, iter);
+								if (path != null)
+									iFolderListStore.EmitRowChanged(path, iter);
 							}
-//							else
-//							{
-//Console.WriteLine("*** SOMETHING WENT BAD IN iFolderData.Refresh() ***");
-//							}
 						}
 						else
 						{
@@ -629,12 +626,7 @@ namespace Novell.iFolder
 										new iFolderChangedHandler(
 											path, iter, iFolderListStore);
 									GLib.Idle.Add(changedHandler.IdleHandler);
-//									iFolderListStore.EmitRowChanged(path, iter);
 								}
-//								else
-//								{
-//Console.WriteLine("*** SOMETHING WENT BAD IN iFolderData.ReadiFolder() ***");
-//								}
 							}
 						}
 						else
@@ -758,12 +750,7 @@ namespace Novell.iFolder
 										new iFolderChangedHandler(
 											path, iter, iFolderListStore);
 									GLib.Idle.Add(changedHandler.IdleHandler);
-//									iFolderListStore.EmitRowChanged(path, iter);
 								}
-//								else
-//								{
-//Console.WriteLine("*** SOMETHING WENT BAD IN iFolderData.ReadAvailableiFolder() ***");
-//								}
 							}
 						}
 					}
@@ -879,9 +866,7 @@ namespace Novell.iFolder
 	    			iFolderWeb reviFolder = 
 						ifws.RevertiFolder(ifHolder.iFolder.ID);
 					if (reviFolder == null)
-					{
-						return null;
-					}
+						throw new Exception(Util.GS("The iFolder Web Service returned a null object when calling RevertiFolder().  You may want to restart your iFolder Client and attempt this operation again."));
 
 					ifHolder.iFolder = reviFolder;
 					if(reviFolder.IsSubscription)
@@ -894,7 +879,10 @@ namespace Novell.iFolder
 					// FIXME: Figure out if there's a better way to cause the UI to update besides causing a Refresh
 					Refresh();
 				}
-				catch{}
+				catch(Exception e)
+				{
+					throw new Exception(Util.GS("An exception occurred while attempting to revert the iFolder to a normal folder.  Exception message: ") + e.Message);
+				}
 
 				return ifHolder;
 			}
@@ -1091,10 +1079,6 @@ namespace Novell.iFolder
 			TreePath path = iFolderListStore.GetPath(iter);
 			if (path != null)
 				iFolderListStore.EmitRowChanged(path, iter);
-//			else
-//			{
-//Console.WriteLine("*** SOMETHING WENT BAD IN iFolderData.OniFolderSyncEvent() ***");
-//			}
 		}
 
 		private void OniFolderFileSyncEvent(object o, FileSyncEventArgs args)
@@ -1147,8 +1131,6 @@ namespace Novell.iFolder
 						TreePath path = iFolderListStore.GetPath(iter);
 						if (path != null)
 							iFolderListStore.EmitRowChanged(path, iter);
-//						else
-//Console.WriteLine("*** SOMETHING WENT BAD IN iFolderData.OniFolderFileSyncEvent() ***");
 					}
 				}
 			}
@@ -1265,10 +1247,8 @@ namespace Novell.iFolder
 		
 		public bool IdleHandler()
 		{
-			if (list != null && path != null)
-			{
+			if (list != null && path != null && list.IterIsValid(iter))
 				list.EmitRowChanged(path, iter);
-			}
 
 			return false;	// Don't keep calling this
 		}
