f87901a2855a1f633a2f8ee150d01a4d7a88565e
btimothy <btimothy@db2431b9-df06-0410-971c-a38ea9a75485>
2006-05-23 23:14:51 +0000

    Added all the widgets on the account properties pages and stubbed out the initializers, but we're going to need to implement more in the API and figure out where the disk space quota will come from.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@6332 db2431b9-df06-0410-971c-a38ea9a75485

--- client/src/trayapp/linux/gnome-applet/account-properties-dialog.c
+++ client/src/trayapp/linux/gnome-applet/account-properties-dialog.c
@@ -55,6 +55,8 @@ struct _IFAAccountPropsDialogPrivate
 	/**
 	 * Widgets for the Server Page
 	 */
+	GtkWidget	*serverPage;
+	GtkWidget	*serverNameLabel;
 	GtkWidget	*serverAddressEntry;
 	GtkWidget	*serverDescriptionTextView;
 	gboolean	bServerAddressChanged;
@@ -62,6 +64,8 @@ struct _IFAAccountPropsDialogPrivate
 	/**
 	 * Widgets for the Identity Page
 	 */
+	GtkWidget	*identityPage;
+	GtkWidget	*userNameLabel;
 	GtkWidget	*passwordEntry;
 	GtkWidget	*rememberPasswordButton;
 	gboolean	bPasswordChanged;
@@ -69,6 +73,7 @@ struct _IFAAccountPropsDialogPrivate
 	/**
 	 * Widgets for the Disk Space Page
 	 */
+	GtkWidget	*diskSpacePage;
 	GtkWidget	*quotaTotalLabel;
 	GtkWidget	*quotaUsedLabel;
 	GtkWidget	*quotaAvailableLabel;
@@ -109,6 +114,12 @@ static void on_key_press (GtkWidget *widget, GdkEventKey *event, IFAAccountProps
 static void on_key_release (GtkWidget *widget, GdkEventKey *event, IFAAccountPropsDialog *apd);
 static void on_realize (GtkWidget *widget, IFAAccountPropsDialog *apd);
 
+static void on_server_page_realize (GtkWidget *widget, IFAAccountPropsDialog *apd);
+static void on_identity_page_realize (GtkWidget *widget, IFAAccountPropsDialog *apd);
+static void on_disk_space_page_realize (GtkWidget *widget, IFAAccountPropsDialog *apd);
+
+static void	init_global_check_buttons (IFAAccountPropsDialog *apd);
+
 
 static void
 ifa_account_props_dialog_class_init (IFAAccountPropsDialogClass *klass)
@@ -116,6 +127,8 @@ ifa_account_props_dialog_class_init (IFAAccountPropsDialogClass *klass)
   GObjectClass *object_class;
   GtkWidgetClass *widget_class;
   GtkDialogClass *dialog_class;
+  
+  g_message ("ifa_account_props_dialog_class_init()");
 	
   object_class = (GObjectClass *)klass;
   widget_class = (GtkWidgetClass *)klass;
@@ -136,6 +149,8 @@ ifa_account_props_dialog_init (IFAAccountPropsDialog *account_props_dialog)
 {
 	IFAAccountPropsDialogPrivate *priv;
 	GtkWidget *vbox;
+	
+	g_message ("ifa_account_props_dialog_init()");
 
 	/* Data */
 	priv = IFA_ACCOUNT_PROPS_DIALOG_GET_PRIVATE (account_props_dialog);
@@ -239,7 +254,10 @@ ifa_account_props_dialog_new(GtkWindow *parent, iFolderDomain *domain)
 {
 	IFAAccountPropsDialogPrivate *priv;
 	gchar *tmpStr;
+	g_message ("ifa_account_props_dialog_new()...calling g_object_new()");
 	IFAAccountPropsDialog *dialog = IFA_ACCOUNT_PROPS_DIALOG (g_object_new (IFA_ACCOUNT_PROPS_DIALOG_TYPE, NULL));
+	
+	g_message ("done calling g_object_new()");
 	
 	priv = IFA_ACCOUNT_PROPS_DIALOG_GET_PRIVATE(dialog);
 
@@ -266,6 +284,10 @@ ifa_account_props_dialog_new(GtkWindow *parent, iFolderDomain *domain)
 	g_signal_connect (dialog, "key-press-event", G_CALLBACK (on_key_press), dialog);
 	g_signal_connect (dialog, "key-release-event", G_CALLBACK (on_key_release), dialog);
 	g_signal_connect (dialog, "realize", G_CALLBACK (on_realize), dialog);
+	
+	g_signal_connect (priv->serverPage, "realize", G_CALLBACK (on_server_page_realize), dialog);
+	g_signal_connect (priv->identityPage, "realize", G_CALLBACK (on_identity_page_realize), dialog);
+	g_signal_connect (priv->diskSpacePage, "realize", G_CALLBACK (on_disk_space_page_realize), dialog);
 
 	return GTK_WIDGET (dialog);
 }
@@ -300,17 +322,19 @@ create_notebook (IFAAccountPropsDialog *apd)
 GtkWidget *
 create_server_page (IFAAccountPropsDialog *apd)
 {
-	GtkWidget *vbox;
 	GtkWidget *table;
 	GtkWidget *l;
 	IFAAccountPropsDialogPrivate *priv;
+	GtkRequisition req;
+	GtkWidget *sw;
+	GtkTextBuffer *textBuffer;
 
 	priv = IFA_ACCOUNT_PROPS_DIALOG_GET_PRIVATE (apd);
 
-	vbox = gtk_vbox_new (FALSE, 0);
+	priv->serverPage = gtk_vbox_new (FALSE, 0);
 
 	table = gtk_table_new(6, 2, FALSE);
-	gtk_box_pack_start (GTK_BOX (vbox), table, true, true, 0);
+	gtk_box_pack_start (GTK_BOX (priv->serverPage), table, true, true, 0);
 	gtk_table_set_col_spacings(GTK_TABLE(table), 12);
 	gtk_table_set_row_spacings(GTK_TABLE(table), 6);
 	gtk_container_set_border_width(GTK_CONTAINER(table), 12);
@@ -325,13 +349,13 @@ create_server_page (IFAAccountPropsDialog *apd)
 					 0,0);
 	gtk_misc_set_alignment(GTK_MISC(l), 0, 0.5);
 
-	l = gtk_label_new (ifolder_domain_get_name (priv->domain));
-	gtk_table_attach (GTK_TABLE (table), l, 1,2, 0,1,
+	priv->serverNameLabel = gtk_label_new (NULL);
+	gtk_table_attach (GTK_TABLE (table), priv->serverNameLabel, 1,2, 0,1,
 					 (GtkAttachOptions)(GTK_EXPAND | GTK_FILL),
 					 (GtkAttachOptions)0,
 					 0,0);
-	gtk_label_set_use_underline (GTK_LABEL (l), FALSE);
-	gtk_misc_set_alignment(GTK_MISC(l), 0, 0.5);
+	gtk_label_set_use_underline (GTK_LABEL (priv->serverNameLabel), FALSE);
+	gtk_misc_set_alignment(GTK_MISC(priv->serverNameLabel), 0, 0.5);
 
 	/**
 	 * Row 2
@@ -342,38 +366,241 @@ create_server_page (IFAAccountPropsDialog *apd)
 					(GtkAttachOptions)0,
 					0,0);
 	gtk_misc_set_alignment(GTK_MISC(l), 0, 0);
+	gtk_label_set_use_underline (GTK_LABEL (l), FALSE);
 	
+	priv->serverAddressEntry = gtk_entry_new ();
+	gtk_table_attach (GTK_TABLE (table), priv->serverAddressEntry, 1,2, 1,2,
+					 (GtkAttachOptions)(GTK_SHRINK | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_label_set_mnemonic_widget (GTK_LABEL (l), priv->serverAddressEntry);
 
-	return vbox;
+	/**
+	 * Row 3
+	 */
+	l = gtk_label_new (_("You can change the address if you are offline"));
+	gtk_table_attach (GTK_TABLE (table), l, 1,2, 2,3,
+					(GtkAttachOptions)(GTK_SHRINK | GTK_FILL),
+					(GtkAttachOptions)0,
+					0,0);
+	gtk_label_set_line_wrap (GTK_LABEL (l), TRUE);
+	gtk_misc_set_alignment(GTK_MISC(l), 0, 0.5);
+	gtk_widget_set_sensitive (l, FALSE);
+	gtk_widget_size_request (priv->serverAddressEntry, &req);
+	gtk_widget_set_size_request (l, req.width, -1);
+	
+	/**
+	 * Row 4
+	 */
+	sw = gtk_scrolled_window_new(NULL, NULL);
+	gtk_table_attach (GTK_TABLE (table), sw, 0,2, 3,4,
+					(GtkAttachOptions)(GTK_EXPAND | GTK_FILL),
+					(GtkAttachOptions)0,
+					0,0);
+	gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(sw), GTK_SHADOW_ETCHED_IN);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(sw), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+
+	priv->serverDescriptionTextView = gtk_text_view_new ();
+	gtk_text_view_set_wrap_mode (GTK_TEXT_VIEW (priv->serverDescriptionTextView), GTK_WRAP_WORD);
+	gtk_text_view_set_editable (GTK_TEXT_VIEW (priv->serverDescriptionTextView), FALSE);
+	gtk_widget_set_sensitive (priv->serverDescriptionTextView, FALSE);
+	gtk_text_view_set_cursor_visible (GTK_TEXT_VIEW (priv->serverDescriptionTextView), FALSE);
+	gtk_text_view_set_left_margin (GTK_TEXT_VIEW (priv->serverDescriptionTextView), 5);
+	gtk_text_view_set_right_margin (GTK_TEXT_VIEW (priv->serverDescriptionTextView), 5);
+	gtk_container_add(GTK_CONTAINER(sw), priv->serverDescriptionTextView);
+
+	return priv->serverPage;
 }
 
 GtkWidget *
 create_identity_page (IFAAccountPropsDialog *apd)
 {
-	GtkWidget *vbox;
+	GtkWidget *table;
+	GtkWidget *l;
+	IFAAccountPropsDialogPrivate *priv;
 
-	vbox = gtk_vbox_new (FALSE, 0);
+	priv = IFA_ACCOUNT_PROPS_DIALOG_GET_PRIVATE (apd);
 
-	return vbox;
+	priv->identityPage = gtk_vbox_new (FALSE, 0);
+
+	table = gtk_table_new(3, 2, FALSE);
+	gtk_box_pack_start (GTK_BOX (priv->identityPage), table, true, true, 0);
+	gtk_table_set_col_spacings(GTK_TABLE(table), 12);
+	gtk_table_set_row_spacings(GTK_TABLE(table), 6);
+	gtk_container_set_border_width(GTK_CONTAINER(table), 12);
+
+	/**
+	 * Row 1 (User Name)
+	 */
+	l = gtk_label_new (_("User Name:"));
+	gtk_table_attach (GTK_TABLE (table), l, 0,1, 0,1,
+					 (GtkAttachOptions)(GTK_SHRINK | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_misc_set_alignment(GTK_MISC(l), 0, 0.5);
+
+	priv->userNameLabel = gtk_label_new (NULL);
+	gtk_table_attach (GTK_TABLE (table), l, 1,2, 0,1,
+					 (GtkAttachOptions)(GTK_EXPAND | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_label_set_use_underline (GTK_LABEL (l), FALSE);
+	gtk_misc_set_alignment(GTK_MISC(l), 0, 0.5);
+	
+	/**
+	 * Row 2 (User Password)
+	 */
+	l = gtk_label_new (_("_Password:"));
+	gtk_table_attach (GTK_TABLE (table), l, 0,1, 1,2,
+					 (GtkAttachOptions)(GTK_SHRINK | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_misc_set_alignment(GTK_MISC(l), 0, 0.5);
+	gtk_label_set_use_underline (GTK_LABEL (l), TRUE);
+
+	priv->passwordEntry = gtk_entry_new ();
+	gtk_table_attach (GTK_TABLE (table), priv->passwordEntry, 1,2, 1,2,
+					 (GtkAttachOptions)(GTK_SHRINK | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_entry_set_visibility (GTK_ENTRY (priv->passwordEntry), FALSE);
+	gtk_label_set_mnemonic_widget (GTK_LABEL (l), priv->passwordEntry);
+	
+	/**
+	 * Row 3 (Remember Password Checkbox)
+	 */
+	priv->rememberPasswordButton = gtk_check_button_new_with_mnemonic (_("_Remember password"));
+	gtk_table_attach (GTK_TABLE (table), priv->passwordEntry, 1,2, 2,3,
+					 (GtkAttachOptions)(GTK_EXPAND | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+
+	return priv->identityPage;
 }
 
 GtkWidget *
 create_disk_space_page (IFAAccountPropsDialog *apd)
 {
-	GtkWidget *vbox;
+	GtkWidget *table;
+	GtkWidget *l;
+	IFAAccountPropsDialogPrivate *priv;
+	GtkWidget *graphFrame;
+	GtkWidget *graphBox;
+	GtkWidget *graphLabelBox;
 
-	vbox = gtk_vbox_new (FALSE, 0);
+	priv = IFA_ACCOUNT_PROPS_DIALOG_GET_PRIVATE (apd);
 
-	return vbox;
+	priv->diskSpacePage = gtk_vbox_new (FALSE, 0);
+
+	table = gtk_table_new(3, 3, FALSE);
+	gtk_box_pack_start (GTK_BOX (priv->diskSpacePage), table, true, true, 0);
+	gtk_table_set_col_spacings(GTK_TABLE(table), 12);
+	gtk_table_set_row_spacings(GTK_TABLE(table), 6);
+	gtk_container_set_border_width(GTK_CONTAINER(table), 12);
+
+	/**
+	 * Row 1 (Quota)
+	 */
+	l = gtk_label_new (_("Quota:"));
+	gtk_table_attach (GTK_TABLE (table), l, 0,1, 0,1,
+					 (GtkAttachOptions)(GTK_EXPAND | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_misc_set_alignment(GTK_MISC(l), 0, 0.5);
+
+	priv->quotaTotalLabel = gtk_label_new (NULL);
+	gtk_table_attach (GTK_TABLE (table), priv->quotaTotalLabel, 1,2, 0,1,
+					 (GtkAttachOptions)(GTK_SHRINK | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_misc_set_alignment(GTK_MISC(priv->quotaTotalLabel), 0, 0.5);
+	
+	/**
+	 * Row 2 (Used)
+	 */
+	l = gtk_label_new (_("Used:"));
+	gtk_table_attach (GTK_TABLE (table), l, 0,1, 1,2,
+					 (GtkAttachOptions)(GTK_EXPAND | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_misc_set_alignment(GTK_MISC(l), 0, 0.5);
+
+	priv->quotaUsedLabel = gtk_label_new (NULL);
+	gtk_table_attach (GTK_TABLE (table), priv->quotaUsedLabel, 1,2, 1,2,
+					 (GtkAttachOptions)(GTK_SHRINK | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_misc_set_alignment(GTK_MISC(priv->quotaUsedLabel), 0, 0.5);
+	
+	/**
+	 * Row 3 (Available)
+	 */
+	l = gtk_label_new (_("Available:"));
+	gtk_table_attach (GTK_TABLE (table), l, 0,1, 2,3,
+					 (GtkAttachOptions)(GTK_EXPAND | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_misc_set_alignment(GTK_MISC(l), 0, 0.5);
+
+	priv->quotaAvailableLabel = gtk_label_new (NULL);
+	gtk_table_attach (GTK_TABLE (table), priv->quotaAvailableLabel, 1,2, 1,2,
+					 (GtkAttachOptions)(GTK_SHRINK | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_misc_set_alignment(GTK_MISC(priv->quotaAvailableLabel), 0, 0.5);
+	
+	/**
+	 * Disk Space Graph
+	 */
+	graphFrame = gtk_frame_new (NULL);
+	gtk_table_attach (GTK_TABLE (table), graphFrame, 2,3, 0,3,
+					 (GtkAttachOptions)(GTK_SHRINK | GTK_FILL),
+					 (GtkAttachOptions)0,
+					 0,0);
+	gtk_frame_set_shadow_type (GTK_FRAME (graphFrame), GTK_SHADOW_ETCHED_OUT);
+	
+	graphBox = gtk_hbox_new (FALSE, 5);
+	gtk_container_set_border_width (GTK_CONTAINER (graphBox), 5);
+	
+	gtk_container_add (GTK_CONTAINER (graphFrame), graphBox);
+	
+	priv->quotaGraph = gtk_progress_bar_new ();
+	gtk_box_pack_start (GTK_BOX (graphBox), priv->quotaGraph, FALSE, TRUE, 0);
+
+	gtk_progress_bar_set_orientation (GTK_PROGRESS_BAR (priv->quotaGraph), GTK_PROGRESS_BOTTOM_TO_TOP);
+	gtk_progress_bar_set_pulse_step (GTK_PROGRESS_BAR (priv->quotaGraph), 0.10);
+	gtk_progress_bar_set_fraction (GTK_PROGRESS_BAR (priv->quotaGraph), 0);
+	
+	graphLabelBox = gtk_vbox_new (FALSE, 0);
+	gtk_box_pack_start (GTK_BOX (graphBox), graphLabelBox, FALSE, TRUE, 0);
+	
+	l = gtk_label_new (_("full"));
+	gtk_misc_set_alignment(GTK_MISC(l), 0, 0);
+	gtk_box_pack_start (GTK_BOX (graphLabelBox), l, TRUE, TRUE, 0);
+	
+	l = gtk_label_new (_("empty"));
+	gtk_misc_set_alignment(GTK_MISC(l), 0, 1);
+	gtk_box_pack_start (GTK_BOX (graphLabelBox), l, TRUE, TRUE, 0);
+	
+	return priv->diskSpacePage;
 }
 
 GtkWidget *
 create_global_check_buttons (IFAAccountPropsDialog *apd)
 {
+	IFAAccountPropsDialogPrivate *priv;
 	GtkWidget *vbox;
 
-	vbox = gtk_vbox_new (FALSE, 0);
+	priv = IFA_ACCOUNT_PROPS_DIALOG_GET_PRIVATE (apd);
 
+	vbox = gtk_vbox_new (FALSE, 0);
+	
+	priv->enableAccountButton = gtk_check_button_new_with_mnemonic (_("_Automatically connect"));
+	gtk_box_pack_start (GTK_BOX (vbox), priv->enableAccountButton, FALSE, FALSE, 0);
+	
+	priv->defaultAccountButton = gtk_check_button_new_with_mnemonic (_("Account is _default"));
+	gtk_box_pack_start (GTK_BOX (vbox), priv->defaultAccountButton, FALSE, FALSE, 0);
+	
 	return vbox;
 }
 
@@ -406,6 +633,152 @@ on_key_release (GtkWidget *widget, GdkEventKey *event, IFAAccountPropsDialog *ap
 static void
 on_realize (GtkWidget *widget, IFAAccountPropsDialog *apd)
 {
+	g_message ("FIXME: Implement IFAAccountPropsDialog::on_realize()");
+
+	/* FIXME: Register for domain events that could affect this dialog */
+	init_global_check_buttons (apd);
 }
 
+static void
+on_server_page_realize (GtkWidget *widget, IFAAccountPropsDialog *apd)
+{
+	IFAAccountPropsDialogPrivate *priv;
+	GtkTextBuffer *textBuffer;
+	
+	g_message ("FIXME: Implement IFAAccountPropsDialog::on_server_page_realize()");
+	
+	priv = IFA_ACCOUNT_PROPS_DIALOG_GET_PRIVATE (apd);
+	
+	if (ifolder_domain_get_name (priv->domain) != NULL)
+		gtk_label_set_text (GTK_LABEL (priv->serverNameLabel), ifolder_domain_get_name (priv->domain));
+		
+	if (ifolder_domain_get_host_address (priv->domain) != NULL)
+		gtk_entry_set_text (GTK_ENTRY (priv->serverAddressEntry), ifolder_domain_get_host_address (priv->domain));
+		
+	if (ifolder_domain_is_authenticated (priv->domain))
+	{
+		gtk_widget_set_sensitive (priv->serverAddressEntry, FALSE);
+		gtk_entry_set_editable (GTK_ENTRY (priv->serverAddressEntry), FALSE);
+	}
+	else
+	{
+		gtk_widget_set_sensitive (priv->serverAddressEntry, TRUE);
+		gtk_entry_set_editable (GTK_ENTRY (priv->serverAddressEntry), TRUE);
+	}
+
+	if (ifolder_domain_get_description (priv->domain) != NULL)
+	{
+		textBuffer = gtk_text_buffer_new (NULL);
+		gtk_text_buffer_set_text (textBuffer, ifolder_domain_get_description (priv->domain), -1);
+		gtk_text_view_set_buffer (GTK_TEXT_VIEW (priv->serverDescriptionTextView), textBuffer);
+	}
+}
+
+static void
+on_identity_page_realize (GtkWidget *widget, IFAAccountPropsDialog *apd)
+{
+	IFAAccountPropsDialogPrivate *priv;
+
+	g_message ("FIXME: Implement IFAAccountPropsDialog::on_identity_page_realize()");
+
+	priv = IFA_ACCOUNT_PROPS_DIALOG_GET_PRIVATE (apd);
+
+	if (ifolder_domain_get_user_name (priv->domain) != NULL)
+		gtk_label_set_text (GTK_LABEL (priv->userNameLabel), ifolder_domain_get_user_name (priv->domain));
+	
+	g_debug ("FIXME: Read in the user's password in IFAAccountPropsDialog::on_identity_page_realize()");
+	/*
+	if (ifolder_domain_get_user_password (priv->domain) != NULL)
+	{
+		gtk_entry_set_text (GTK_ENTRY (priv->passwordEntry), ifolder_domain_get_user_password (priv->domain));
+		
+		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (priv->rememberPasswordButton), TRUE);
+	}
+	else
+	{
+		gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (priv->rememberPasswordButton), FALSE);
+	}
+	*/
+}
+
+static void
+on_disk_space_page_realize (GtkWidget *widget, IFAAccountPropsDialog *apd)
+{
+	g_message ("FIXME: Implement IFAAccountPropsDialog::on_disk_space_page_realize()");
+/*
+			DiskSpace ds = ifdata.GetUserDiskSpace(domain.MemberUserID);
+
+			if(ds == null)
+			{
+				QuotaTotalLabel.Text = Util.GS("N/A");
+				QuotaUsedLabel.Text = Util.GS("N/A");
+				QuotaAvailableLabel.Text = Util.GS("N/A");
+				QuotaGraph.Fraction = 0;
+			}
+			else
+			{
+				int tmpValue;
+
+				if(ds.Limit == 0)
+				{
+					QuotaTotalLabel.Text = Util.GS("N/A");
+				}
+				else
+				{
+					tmpValue = (int)(ds.Limit / (1024 * 1024));
+					QuotaTotalLabel.Text =
+						string.Format("{0} {1}", tmpValue, Util.GS("MB"));
+				}
+
+				if(ds.UsedSpace == 0)
+				{
+					QuotaUsedLabel.Text = Util.GS("N/A");
+				}
+				else
+				{
+					tmpValue = (int)(ds.UsedSpace / (1024 * 1024)) + 1;
+					QuotaUsedLabel.Text =
+						string.Format("{0} {1}", tmpValue, Util.GS("MB"));
+				}
+
+				if(ds.AvailableSpace == 0)
+				{
+					QuotaAvailableLabel.Text = Util.GS("N/A");
+				}
+				else
+				{
+					tmpValue = (int)(ds.AvailableSpace / (1024 * 1024));
+					QuotaAvailableLabel.Text =
+						string.Format("{0} {1}",tmpValue, Util.GS("MB"));
+				}
+
+				if(ds.Limit == 0)
+				{
+					QuotaGraph.Fraction = 0;
+				}
+				else
+				{
+					if(ds.Limit < ds.UsedSpace)
+						QuotaGraph.Fraction = 1;
+					else
+						QuotaGraph.Fraction = ((double)ds.UsedSpace) / 
+												((double)ds.Limit);
+				}
+			}
+*/
+}
+
+static void
+init_global_check_buttons (IFAAccountPropsDialog *apd)
+{
+	IFAAccountPropsDialogPrivate *priv;
+
+	g_message ("FIXME: Implement IFAAccountPropsDialog::init_global_check_buttons()");
+
+	priv = IFA_ACCOUNT_PROPS_DIALOG_GET_PRIVATE (apd);
+	
+	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (priv->enableAccountButton), ifolder_domain_is_active (priv->domain));
+	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (priv->defaultAccountButton), ifolder_domain_is_default (priv->domain));
+	gtk_widget_set_sensitive (priv->defaultAccountButton, !(ifolder_domain_is_default (priv->domain)));
+}
 
