509f37b5d248ddd6de13e417281b5d4437374ec7
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2006-05-23 15:30:33 +0000

    updated to have a quit button and to init threads
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@6327 db2431b9-df06-0410-971c-a38ea9a75485

--- client/src/trayapp/windows/main.c
+++ client/src/trayapp/windows/main.c
@@ -6,13 +6,9 @@
 
 iFolderClient *ifolder_client = NULL;
 
-/* This is a callback function. The data arguments are ignored
- * in this example. More on callbacks below. */
-static void hello( GtkWidget *widget,
-                   gpointer   data )
+static void showPreferences( GtkWidget *widget, gpointer data )
 {
 	ifa_show_preferences_window(1);
-    g_print ("Hello World\n");
 }
 
 static gboolean delete_event( GtkWidget *widget,
@@ -47,10 +43,10 @@ int main( int   argc,
 
     /* GtkWidget is the storage type for widgets */
     GtkWidget *window;
-    GtkWidget *button;
-    
-    /* This is called in all GTK applications. Arguments are parsed
-     * from the command line and are returned to the application. */
+    GtkWidget *prefsButton, *quitButton;
+
+	g_thread_init(NULL);
+
     gtk_init (&argc, &argv);
 
 	if (argc < 2)
@@ -67,17 +63,9 @@ int main( int   argc,
     /* create a new window */
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
     
-    /* When the window is given the "delete_event" signal (this is given
-     * by the window manager, usually by the "close" option, or on the
-     * titlebar), we ask it to call the delete_event () function
-     * as defined above. The data passed to the callback
-     * function is NULL and is ignored in the callback function. */
     g_signal_connect (G_OBJECT (window), "delete_event",
 		      G_CALLBACK (delete_event), NULL);
     
-    /* Here we connect the "destroy" event to a signal handler.  
-     * This event occurs when we call gtk_widget_destroy() on the window,
-     * or if we return FALSE in the "delete_event" callback. */
     g_signal_connect (G_OBJECT (window), "destroy",
 		      G_CALLBACK (destroy), NULL);
     
@@ -85,33 +73,31 @@ int main( int   argc,
     gtk_container_set_border_width (GTK_CONTAINER (window), 10);
     
     /* Creates a new button with the label "Hello World". */
-    button = gtk_button_new_with_label ("Hello World");
+    prefsButton = gtk_button_new_with_label ("Preferences");
     
-    /* When the button receives the "clicked" signal, it will call the
-     * function hello() passing it NULL as its argument.  The hello()
-     * function is defined above. */
-    g_signal_connect (G_OBJECT (button), "clicked",
-		      G_CALLBACK (hello), NULL);
+    g_signal_connect (G_OBJECT (prefsButton), "clicked",
+		      G_CALLBACK (showPreferences), NULL);
+
+    /* Creates a new button with the label "Hello World". */
+    quitButton = gtk_button_new_with_label ("Quit");
     
-    /* This will cause the window to be destroyed by calling
-     * gtk_widget_destroy(window) when "clicked".  Again, the destroy
-     * signal could come from here, or the window manager. */
- //   g_signal_connect_swapped (G_OBJECT (button), "clicked",
-	//		      G_CALLBACK (gtk_widget_destroy),
-      //                        G_OBJECT (window));
-    
-    /* This packs the button into the window (a gtk container). */
-    gtk_container_add (GTK_CONTAINER (window), button);
-    
-    /* The final step is to display this newly created widget. */
-    gtk_widget_show (button);
-    
-    /* and the window */
-    gtk_widget_show (window);
-    
-    /* All GTK applications must have a gtk_main(). Control ends here
-     * and waits for an event to occur (like a key press or
-     * mouse event). */
+    g_signal_connect (G_OBJECT (quitButton), "clicked",
+					  G_CALLBACK (showPreferences), NULL);
+	g_signal_connect_swapped (G_OBJECT (quitButton), "clicked",
+							  G_CALLBACK (gtk_widget_destroy),
+							  G_OBJECT (window));
+
+	GtkWidget *winBox = gtk_vbox_new(false, 7);
+	gtk_container_set_border_width(GTK_CONTAINER(winBox), 7);
+
+	gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+	gtk_container_add(GTK_CONTAINER(window), winBox);
+
+	gtk_box_pack_start(GTK_BOX(winBox), prefsButton, true, true, 0);
+	gtk_box_pack_start(GTK_BOX(winBox), quitButton, true, true, 0);
+
+    gtk_widget_show_all (window);
+
     gtk_main ();
 
 	ifolder_client_uninitialize(ifolder_client, &err);
