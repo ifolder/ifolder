cbd4f76821e4f39ac3ba2c111aa6398866eef7d4
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2006-05-25 15:11:33 +0000

    added English.lproj back in the way it should be
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@6339 db2431b9-df06-0410-971c-a38ea9a75485

--- client/src/trayapp/darwin/main.m
+++ client/src/trayapp/darwin/main.m
@@ -1,25 +1,10 @@
-/***********************************************************************
- *  main.m
- * 
- *  Copyright (C) 2006 Novell, Inc.
- *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public
- *  License as published by the Free Software Foundation; either
- *  version 2 of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Library General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- *  Author: Calvin Gaisford <cgaisford@novell.com>
- * 
- ***********************************************************************/
+//
+//  main.m
+//  iFolder3
+//
+//  Created by Calvin Gaisford on 5/25/06.
+//  Copyright __MyCompanyName__ 2006. All rights reserved.
+//
 
 #import <Cocoa/Cocoa.h>
 
