09a10523956bd49d276f060af0bc50a084845e4b
cgaisford <cgaisford@db2431b9-df06-0410-971c-a38ea9a75485>
2006-05-25 14:49:04 +0000

    Added new OS X Project
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@6338 db2431b9-df06-0410-971c-a38ea9a75485

---
+++ client/src/trayapp/darwin/iFolder3App.h
@@ -0,0 +1,50 @@
+/***********************************************************************
+ *  iFolder3App.h
+ * 
+ *  Copyright (C) 2006 Novell, Inc.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Author: Calvin Gaisford <cgaisford@novell.com>
+ * 
+ ***********************************************************************/
+
+#import <Cocoa/Cocoa.h>
+
+@interface iFolder3App : NSObject
+{
+	NSStatusItem		*appStatusItem;
+    IBOutlet NSMenu		*appMenu;
+}
+
+//==========================================
+// IBAction Methods
+//==========================================
+- (IBAction)showAbout:(id)sender;
+- (IBAction)showHelp:(id)sender;
+- (IBAction)showiFolders:(id)sender;
+- (IBAction)showPreferences:(id)sender;
+- (IBAction)startFullSync:(id)sender;
+- (IBAction)stopSync:(id)sender;
+
+
+//==========================================
+// NSApplication Delegates
+//==========================================
+- (void)applicationDidFinishLaunching:(NSNotification*)notification;
+- (void)applicationWillTerminate:(NSNotification *)notification;
+
+
+@end
