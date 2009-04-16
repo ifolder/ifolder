f8a52e6de774885da11a93e326e301e0ec6ac05a
kalidasbala <kalidasbala@db2431b9-df06-0410-971c-a38ea9a75485>
2009-04-16 16:57:37 +0000

    Reset password update
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk/ifolder@6730 db2431b9-df06-0410-971c-a38ea9a75485

--- src/UI/Windows/FormsTrayApp/ResetPassword.cs
+++ src/UI/Windows/FormsTrayApp/ResetPassword.cs
@@ -103,6 +103,38 @@ namespace Novell.FormsTrayApp
 			}
 		}
 
+        public ResetPassword(SimiasWebService simws, iFolderWebService ifws)
+        {
+            //
+            // Required for Windows Form Designer support
+            //
+            InitializeComponent();
+            this.simws = simws;
+            this.ifws = ifws;
+            GetLoggedInDomains();
+            //
+            // TODO: Add any constructor code after InitializeComponent call
+            //
+        }
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		protected override void Dispose( bool disposing )
+		{
+			if( disposing )
+			{
+				if(components != null)
+				{
+					components.Dispose();
+				}
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent()
