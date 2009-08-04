e9e2ace3fbf22fb48a299fc3305ba0e9ea2b309a
sbipin <sbipin@db2431b9-df06-0410-971c-a38ea9a75485>
2009-08-04 09:11:17 +0000

    Checkin in localized ui folder.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@7039 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/help/ja/ui/config.js
+++ ifolder/help/ja/ui/config.js
@@ -1,7 +1,7 @@
 var CFG_MAIN_TOPIC="./bookinfo.html";
 var CFG_HTML2UI_RELPATH="ui/";
 var CFG_HTML2IDX_RELPATH="./";
-var CFG_LANG_ID="en";
+var CFG_LANG_ID="ja";
 var CFG_RTL_TEXT=false;
 var CFG_HAS_TOC=true;
 var CFG_HAS_SEARCH=true;
