ec97ef5935c79b7d89f2a2804210625a4071e4b4
sbipin <sbipin@db2431b9-df06-0410-971c-a38ea9a75485>
2010-08-04 10:20:53 +0000

    ID: #00000 Reviewer: Shane Localization Required: No Documentation Required: No Description: Localized help files included.
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/iFolder_3.8.2_beta@7423 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/help/sk/ui/resource.js
+++ ifolder/help/sk/ui/resource.js
@@ -1,10 +1,10 @@
-﻿var RES_PREV_STR="predchádzajúce";
-var RES_NEXT_STR="nasledujúce";
+﻿var RES_PREV_STR="naspäť";
+var RES_NEXT_STR="ďalej";
 var RES_CONTENTS_STR = "Obsah";
 var RES_INDEX_STR = "Index";
 var RES_SEARCH_STR = "Hľadať";
 var RES_WORDSEPREGEX="[\\s -:;,\\[\\]<>{}&'\"!\?|()/\\\\]";
-var RES_COMMONWORDS="";
+var RES_COMMONWORDS="a about an are as at be by do for from how I in is it of on that the this to was what when where who will with";
 var RES_STEMMING="";
 var RES_SPELLING="";
 var RES_MSGNOTFOUND="Nenašli sa žiadne témy.";
