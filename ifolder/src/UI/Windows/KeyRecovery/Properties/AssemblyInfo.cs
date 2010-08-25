9f99ac74aef0cdbe0b6d8a6470d7287aeef34ec3
ravim85 <ravim85@db2431b9-df06-0410-971c-a38ea9a75485>
2010-08-25 12:42:03 +0000

    Bumping version to 3.8.2.1 - beta2 -iFolder
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/iFolder_3.8.2_beta@7470 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/src/UI/Windows/KeyRecovery/Properties/AssemblyInfo.cs
+++ ifolder/src/UI/Windows/KeyRecovery/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.8.1.1")]
-[assembly: AssemblyFileVersion("3.8.1.1")]
+[assembly: AssemblyVersion("3.8.2.1")]
+[assembly: AssemblyFileVersion("3.8.2.1")]
