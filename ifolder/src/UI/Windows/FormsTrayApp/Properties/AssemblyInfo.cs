defec3997bb7d7f1eac9935de0d6c9f17c408ea1
kuanil <kuanil@db2431b9-df06-0410-971c-a38ea9a75485>
2011-02-23 13:35:22 +0000

    ID:#616747 Reviewer: Vikash Localization Required: No Documentation Required: No Description: version bump for ifolder updates
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/iFolder_3.8.0_updates@7578 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/src/UI/Windows/FormsTrayApp/Properties/AssemblyInfo.cs
+++ ifolder/src/UI/Windows/FormsTrayApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.8.0.7")]
-[assembly: AssemblyFileVersion("3.8.0.7")]
+[assembly: AssemblyVersion("3.8.0.8")]
+[assembly: AssemblyFileVersion("3.8.0.8")]
