67e10c46bd9a94cf74ea9280d6065364cc82635e
spratima <spratima@db2431b9-df06-0410-971c-a38ea9a75485>
2011-06-22 13:35:34 +0000

    ID: #631242 Reviewer: GG Localization Required: No Documentation Required: No Description: GroupWise 8.0.2 Client crash caused by iFolder
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/trunk@7602 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/src/UI/Windows/iFolderShell/iFolderShell/iFolderShell.h
+++ ifolder/src/UI/Windows/iFolderShell/iFolderShell/iFolderShell.h
@@ -92,7 +92,7 @@ protected:
     LPDATAOBJECT m_pDataObj;
 
 public:
-	static IiFolderComponentPtr m_spiFolder;
+	IiFolderComponentPtr m_spiFolder;
     TCHAR m_szFileUserClickedOn[MAX_PATH];
 	static TCHAR m_szShellPath[MAX_PATH];
 	iFolderClass m_iFolderClass;
@@ -141,8 +141,8 @@ public:
 	STDMETHODIMP			GetPriority(int *pIPriority);
 	
 	//IShellPropSheetExt methods
-	STDMETHODIMP			AddPages(LPFNADDPROPSHEETPAGE lpfnAddPage,LPARAM lParam);
-							
+	STDMETHODIMP			AddPages(LPFNADDPROPSHEETPAGE lpfnAddPage,
+									LPARAM lParam);
 	
 	STDMETHODIMP			ReplacePage(UINT uPageID,
 										LPFNADDPROPSHEETPAGE lpfnReplaceWith,
