b778160df72ecd41c5635565fdf313bf74dbb2bf
hegdegg <hegdegg@db2431b9-df06-0410-971c-a38ea9a75485>
2011-09-15 11:35:07 +0000

    ID:#616747 Reviewer: Tuks Localization Required:No Documentation Required:No Description: Update version to 3.8.4.2
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/iFolder_3.8.4_updates@7622 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/src/UI/Windows/KeyRecovery/Properties/AssemblyInfo.cs
+++ ifolder/src/UI/Windows/KeyRecovery/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.8.4.1")]
-[assembly: AssemblyFileVersion("3.8.4.1")]
+[assembly: AssemblyVersion("3.8.4.2")]
+[assembly: AssemblyFileVersion("3.8.4.2")]
