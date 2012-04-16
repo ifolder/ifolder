33b4024b5e3b09c576b0fef2e85925be2949128f
hegdegg <hegdegg@db2431b9-df06-0410-971c-a38ea9a75485>
2012-04-16 13:19:19 +0000

    ID:#712274 Reviewer: Tukaram Localization Required:No Documentation Required:No Description : Replaced depricated API GlobalProxySelection.Select with WebRequest.GetSystemWebProxy().
    git-svn-id: svn://svn.code.sf.net/p/ifolder/code/branches/iFolder_3.9.0_updates@7631 db2431b9-df06-0410-971c-a38ea9a75485

--- ifolder/src/UI/Windows/FormsTrayApp/Preferences.cs
+++ ifolder/src/UI/Windows/FormsTrayApp/Preferences.cs
@@ -1396,7 +1396,7 @@ namespace Novell.FormsTrayApp
             else
             {
                 // Set any proxy information for this domain.
-                IWebProxy iwp = GlobalProxySelection.Select;
+                IWebProxy iwp = WebRequest.GetSystemWebProxy(); 
                 if (!iwp.IsBypassed(ubHost.Uri))
                 {
                     string proxyUser = null;
